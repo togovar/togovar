221a0b78035e93900b69fa2af2a68cc85a456505
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-12 14:22:49 +0900

    add route for ClinVar report

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'static#index'
 
   resources :gene, only: :show, controller: 'reports/gene', constraints: { id: %r{[\w\-:./]+} }
+  resources :clin_var, only: :show, controller: 'reports/clin_var', constraints: { id: %r{\d+} }
 
 end
