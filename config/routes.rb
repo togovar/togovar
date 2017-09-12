063e7c257ddb43830fde05e03436ed3b2f5c8367
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-12 15:46:36 +0900

    add route for ExAC report

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :gene, only: :show, controller: 'reports/gene', constraints: { id: %r{[\w\-:./]+} }
   resources :clin_var, only: :show, controller: 'reports/clin_var', constraints: { id: %r{\d+} }
+  resources :exac, only: :show, controller: 'reports/exac', constraints: { id: /.+/ }
 
 end
