aed1f92c9f98ffffb54e12158f03702646eb77ae
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-12 14:16:05 +0900

    add tag helper for stanza

--- app/models/stanza.rb
+++ app/models/stanza.rb
@@ -1,17 +1,25 @@
-class Stanza < Settingslogic
-  source Rails.root.join(*%w(config stanza.yml)).to_s
+class Stanza < Settingslogic # :nodoc:
   namespace Rails.env
+  source File.join(Rails.root, 'config', 'stanza.yml')
 
-  def ids
-    providers.flat_map {|_report_type, stanzas| stanzas['togostanza'].map {|stanza| stanza['uri'].split('/').last } }
+  # @return [Hash]
+  def all
+    flat_map { |_, stanzas| stanzas.map(&:symbolize_keys) }
   end
 
-  def all
-    providers.flat_map {|report_type, stanzas|
-      stanzas['togostanza'].map {|stanza|
-        id = stanza['uri'].split('/').last
-        stanza.merge('report_type' => report_type, 'id' => id, 'uri' => stanza['uri'])
-      }
-    }
+  # @param [Hash] args
+  # @return [String, nil]
+  def tag(*args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    make_tag("togostanza-#{name}", options)
+  end
+
+  private
+
+  def make_tag(tag_name, options = {})
+    param = options.map do |k, v|
+      %(#{k}="#{v.to_s.gsub('"'.freeze, '&quot;'.freeze)}")
+    end.join(' ')
+    "<#{tag_name}#{" #{param}" if param.present?} />".html_safe
   end
 end
