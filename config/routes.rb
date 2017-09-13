2fb847fbb0c5ad64befddd800a61a3618b6a8215
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-13 10:00:55 +0900

    rename controller of static to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static#index'
+  root 'root#index'
 
   resources :gene, only: :show, controller: 'reports/gene', constraints: { id: %r{[\w\-:./]+} }
   resources :clin_var, only: :show, controller: 'reports/clin_var', constraints: { id: %r{\d+} }
