182c5a3e0bc0c5115fb1240c9f99b839c161809e
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-18 18:58:42 +0900

    add search page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,9 +9,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[bootstrap-combobox.css bootstrap-combobox.js]
 Rails.application.config.assets.precompile << proc do |path|
   if path =~ /\.(css|js)\z/
     full_path = Rails.application.assets.resolve(path)
