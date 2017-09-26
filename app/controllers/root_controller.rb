4bff04bafdc80d2fb931f888a4193c1f3209505e
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-26 12:30:31 +0900

    implement function switches lookup index by current type

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,2 +1,12 @@
 class RootController < ApplicationController
+
+  def suggest
+    case params[:type]
+    when 'disease'
+      render json: Disease.auto_complete(params[:term])
+    else
+      raise("Unknown search type: #{params[:type]}")
+    end
+  end
+
 end
