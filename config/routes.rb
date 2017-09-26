d5e9ce80bdb885e00dbe9e16b0f47a3aacfe025c
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-26 12:26:07 +0900

    add route for suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'root#index'
 
+  get  'disease/suggest', to: 'root#suggest', defaults: { format: 'json' }
+
   resources :gene, only: :show, controller: 'reports/gene', constraints: { id: %r{[\w\-:./]+} }
   resources :clin_var, only: :show, controller: 'reports/clin_var', constraints: { id: %r{\d+} }
   resources :exac, only: :show, controller: 'reports/exac', constraints: { id: /.+/ }
