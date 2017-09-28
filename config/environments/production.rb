b34ae795465beae0d43dbbe00c0c4f0fa6f31745
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-29 03:48:04 +0900

    add redis setting on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,10 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, { host:      'localhost',
+                                       port:      16379,
+                                       db:        0,
+                                       namespace: 'sparql' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
