318a2f58df2eec399ae5095f33dc5e762f2325ed
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-28 16:56:22 +0900

    add route for disease list

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  'disease/suggest', to: 'root#suggest', defaults: { format: 'json' }
 
   get  'report_type/gene', to: 'report_type/gene#list', defaults: { format: 'json' }
+  get  'report_type/disease', to: 'report_type/disease#list', defaults: { format: 'json' }
 
   resources :gene, only: :show, controller: 'reports/gene', constraints: { id: %r{[\w\-:./]+} }
   resources :clin_var, only: :show, controller: 'reports/clin_var', constraints: { id: %r{\d+} }
