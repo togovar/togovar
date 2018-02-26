03decdbba14d73737dffa8e952d0231a95f79f8f
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-02-26 14:24:23 +0900

    fix for basic auth

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -4,5 +4,14 @@ class Endpoint < Settingslogic
   source Rails.root.join('config', 'endpoint.yml').to_s
   namespace Rails.env
 
-  include Queryable
+  def url
+    @url ||= begin
+      url = RDF::URI(endpoint)
+      url.user = user if respond_to?(:user) && user.present?
+      url.password = password if respond_to?(:password) && password.present?
+      url
+    end
+  end
+
+  # include Queryable
 end
