61abba47dde8a12b6177789ca3c58a6a1eac887e
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-12 11:43:54 +0900

    add route for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'root#index'
 
+  get 'search', to: 'root#index'
   get 'suggest', to: 'root#suggest', defaults: { format: 'json' }
   get 'list', to: 'root#list', defaults: { format: 'json' }
 
