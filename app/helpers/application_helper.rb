d76c8c7cb1a509a86f0e307a82cc9854b1a6a328
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-16 15:56:26 +0900

    add function to generate page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper #:nodoc:
+  def title(page_title = '')
+    base_title = 'TogoVar'
+    if page_title.present?
+      page_title + ' | ' + base_title
+    else
+      base_title
+    end
+  end
 end
 
 if defined?(ActionView)
