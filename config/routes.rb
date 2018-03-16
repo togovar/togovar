7ca5edeed54d22b0f55485a683e21f2432b33e40
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-16 15:57:17 +0900

    add routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
   root 'root#index'
 
   get 'search', to: 'root#index'
+
+  # static pages
+  get 'about', to: 'static#about'
+  get 'terms', to: 'static#terms'
+  get 'policy', to: 'static#policy'
+  get 'contact', to: 'static#contact'
+  get 'faq', to: 'static#faq'
+
+  # variation report
+  get 'variation', to: 'reports/variation#show'
+
+  # api
   get 'suggest', to: 'root#suggest', defaults: { format: 'json' }
   get 'list', to: 'root#list', defaults: { format: 'json' }
-
-  get 'variation', to: 'reports/variation#show'
 end
