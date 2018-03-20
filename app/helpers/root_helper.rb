d7d63256eca0230a29d4aa74fef25fdf63a7b7e7
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-20 21:43:43 +0900

    add filter icons

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,2 +1,16 @@
 module RootHelper
+  def filter_label_for(str)
+    case str.downcase
+    when 'snv', 'hgvd'
+      str.upcase
+    when 'togovar'
+      'TogoVar'
+    when 'tommo'
+      'ToMMo 3.5KJPN'
+    when 'exac'
+      'ExAC'
+    else
+      str.humanize
+    end
+  end
 end
