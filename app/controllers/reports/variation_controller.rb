9437ef50ff265c78dde68648183a6c2ec4423798
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-22 08:03:36 +0900

    fix bug

--- app/controllers/reports/variation_controller.rb
+++ app/controllers/reports/variation_controller.rb
@@ -2,7 +2,7 @@ module Reports
   class VariationController < ApplicationController
     def show
       allele_id    = params.permit(:allele_id)[:allele_id]
-      variation_id = Reports::Variation.variation_id_for_allele(@allele_id)
+      variation_id = Reports::Variation.variation_id_for_allele(allele_id)
 
       @param = { clinvar_allele_id: allele_id,
                  clinvar_variation_id: variation_id }
