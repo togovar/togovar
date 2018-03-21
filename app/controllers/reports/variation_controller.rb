8e584f178db6f6426edb89891156db01d54753a9
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-03-22 08:26:09 +0900

    fix bug

--- app/controllers/reports/variation_controller.rb
+++ app/controllers/reports/variation_controller.rb
@@ -4,13 +4,10 @@ module Reports
       allele_id    = params.permit(:allele_id)[:allele_id]
       variation_id = Reports::Variation.variation_id_for_allele(allele_id)
 
-      @param = { clinvar_allele_id: allele_id,
-                 clinvar_variation_id: variation_id }
-
       @stanza = [
-        Stanza::ClinVar.clinvar_variant_information('Variant Information', variation_id: variation_id),
-        Stanza::ClinVar.clinvar_variant_interpretation('Variant Interpretation', variation_id: variation_id),
-        Stanza::ClinVar.clinvar_variant_alleles('Variant Alleles', variation_id: variation_id)
+        Stanza::ClinVar.clinvar_variant_information('Variant Information', clinvar_id: variation_id),
+        Stanza::ClinVar.clinvar_variant_interpretation('Variant Interpretation', clinvar_id: variation_id),
+        Stanza::ClinVar.clinvar_variant_alleles('Variant Alleles', clinvar_id: variation_id)
       ]
     end
   end
