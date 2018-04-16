9a8fd2d70a499c283f432aae2ba0f617811acfd2
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-04-16 13:43:56 +0900

    add stanza

--- app/controllers/reports/variation_controller.rb
+++ app/controllers/reports/variation_controller.rb
@@ -1,14 +1,28 @@
 module Reports
   class VariationController < ApplicationController
     def show
-      allele_id    = params.permit(:allele_id)[:allele_id]
-      variation_id = Reports::Variation.variation_id_for_allele(allele_id)
+      tgv_id = params.permit(:tgv_id)[:tgv_id]
 
-      @stanza = [
-        Stanza::ClinVar.clinvar_variant_information('Variant Information', clinvar_id: variation_id),
-        Stanza::ClinVar.clinvar_variant_interpretation('Variant Interpretation', clinvar_id: variation_id),
-        Stanza::ClinVar.clinvar_variant_alleles('Variant Alleles', clinvar_id: variation_id)
-      ]
+      @stanza = []
+
+      url = "http://togovar.l5dev.jp/sparqlist/api/variant_basic_information?tgv_id=#{tgv_id}"
+      @stanza << Stanza::ClinVar.row_headered_table('Basic Information', url: url)
+
+      lookup = Lookup.find(tgv_id)
+
+      if (allele_id = lookup&.clinvar&.allele_id)
+        variation_id = Reports::Variation.variation_id_for_allele(allele_id)
+        @stanza << Stanza::ClinVar.clinvar_variant_information('Variant Information', clinvar_id: variation_id)
+        @stanza << Stanza::ClinVar.clinvar_variant_interpretation('Variant Interpretation', clinvar_id: variation_id)
+        @stanza << Stanza::ClinVar.clinvar_variant_alleles('Variant Alleles', clinvar_id: variation_id)
+      end
+
+      if (rs = lookup&.base&.rs)
+        Array(rs).each do |x|
+          @stanza << Stanza::ClinVar.pubtator_stanza('Publications', rs: x)
+        end
+      end
+
     end
   end
 end
