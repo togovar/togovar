1a30fb686a9fb358283253f4c5748e32b9401bb7
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-05-14 12:25:42 +0900

    add frequency stanza

--- app/controllers/reports/variation_controller.rb
+++ app/controllers/reports/variation_controller.rb
@@ -6,9 +6,9 @@ module Reports
       @stanza = []
 
       @stanza << Stanza.row_headered_table('', url: "https://togovar.org/sparqlist/api/variant_basic_information?tgv_id=#{id}")
+      @stanza << Stanza.variant_frequency('Frequency', tgv_id: id)
+      @stanza << Stanza.variant_jbrowse('Variant Information', tgv_id: id)
       @stanza << Stanza.column_headered_table('Transcripts', url: "https://togovar.org/sparqlist/api/variant_transcripts?tgv_id=#{id}")
-
-      # @stanza << Stanza.genome_jbrowse('Variant Information', gene_id: 'ADH1A', tax_id: '9606')
 
       lookup = Lookup.find(id)
 
