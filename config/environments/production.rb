8f0d037116ce3a406772835b2ae86c478ed3aac3
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-06-05 02:46:38 +0900

    make host and port configurable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,10 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, { host:      'localhost',
-                                       port:      16379,
+  config.cache_store = :redis_store, { host:      ENV['REDIS_HOST'] || 'localhost',
+                                       port:      ENV['REDIS_PORT'] || 6379,
                                        db:        0,
-                                       namespace: 'sparql' }
+                                       namespace: 'cache' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
