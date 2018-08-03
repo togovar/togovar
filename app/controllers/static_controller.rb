31487be756ab360359cffcd7118d2256c823d5ce
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-08-03 22:57:50 +0900

    implement i18n selector and add English/Japanese pages for static pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,53 @@
 class StaticController < ApplicationController
+  class << self
+    def static_pages
+      @static_pages ||= Dir[static_pages_path]
+                          .map { |x| File.basename(x).split('.').first }
+                          .uniq
+                          .map(&:to_sym)
+    end
+
+    def static_pages_path
+      relative_path = File.join('..', 'views', 'static', '**', '*.html*')
+      File.expand_path(relative_path, File.dirname(__FILE__))
+    end
+  end
+
+  before_action :set_locale, only: static_pages
+
+  static_pages.each do |name|
+    define_method(name) do
+      render file: "static/#{@locale}/#{name}"
+    end
+  end
+
+  private
+
+  def set_locale
+    @locale = cookies[:locale] = I18n.locale = locale
+  end
+
+  def locale
+    locale_from_params || locale_from_cookie || locale_from_request || locale_from_default
+  end
+
+  def locale_from_params
+    (l = params[:locale]).present? ? l.to_sym.presence_in(I18n.available_locales) : nil
+  end
+
+  def locale_from_cookie
+    (l = cookies[:locale]).present? ? l.to_sym.presence_in(I18n.available_locales) : nil
+  end
+
+  def locale_from_request
+    request
+      .env['HTTP_ACCEPT_LANGUAGE'].to_s.split(',')
+      .map { |x| x[0..1].to_sym }
+      .select { |x| I18n.available_locales.include?(x) }
+      .first
+  end
+
+  def locale_from_default
+    I18n.default_locale
+  end
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+I18n.config.available_locales = %i[en ja]
+I18n.enforce_available_locales = true
+I18n.default_locale = :en
