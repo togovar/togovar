f644ee8cfec6901df18b0652e04ade1ca8d3b51a
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-03-24 18:35:16 +0900

    implement functions for search and suggest

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,29 +2,64 @@ class RootController < ApplicationController
   protect_from_forgery except: %i[suggest list]
 
   def suggest
+    term = params.permit(:term)[:term]
+
     respond_to do |format|
-      format.html
       format.json do
-        render json: Suggest.suggest(params.permit(:term)[:term])
+        @response = if term && term.length >= 3
+                      {
+                        gene: GeneSymbol.suggest(term).suggestions.map { |_, v| v.first[:options] }.flatten,
+                        disease: Disease.suggest(term).results
+                      }
+                    else
+                      Hash.new { [] }
+                    end
+
+        render 'suggest', formats: 'json', handlers: 'jbuilder'
       end
     end
   end
 
-  def list
+  BINARY_FILTERS = %i[dataset type significance consequence sift polyphen].freeze
+
+  def search
+    param = Form::VariantSearchParameters.new(search_params)
+
     respond_to do |format|
-      format.html
       format.json do
-        parameters = [:term,
-                      :start,
-                      :length,
-                      source:        [],
-                      variant_type:  [],
-                      freq_source:   [],
-                      freq_relation: [],
-                      freq_value:    [],
-                      significance:  []]
-        render json: Lookup.list(params.permit(*parameters))
+        builder = Elasticsearch::QueryBuilder.new
+
+        if BINARY_FILTERS.map { |x| param.selected_none?(x) }.any?
+          builder.count_only(true)
+        else
+          unless param.selected_all?(:dataset)
+            param.selected_items(:dataset).each do |name|
+              builder.dataset(name)
+            end
+          end
+
+          %i[type significance consequence sift polyphen].each do |x|
+            unless param.selected_all?(x)
+              builder.send(x, *param.selected_items(x))
+            end
+          end
+        end
+
+        if param.debug?
+          render json: builder.build
+          return
+        end
+
+        @response = Variant.search(builder.build)
+        render 'search', formats: 'json', handlers: 'jbuilder'
       end
     end
+  end
+
+  private
+
+  # @return [ActionController::Parameters] parameters
+  def search_params
+    Form::VariantSearchParameters.permit(params)
   end
 end
