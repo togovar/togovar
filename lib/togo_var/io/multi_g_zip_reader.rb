9ce05793d8c1ca705b869969fe8eba0b6afeb166
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-05-07 20:02:46 +0900

    initial import

---
+++ lib/togo_var/io/multi_g_zip_reader.rb
@@ -0,0 +1,58 @@
+require 'zlib'
+
+module TogoVar
+  module IO
+    class MultiGZipReader
+      class << self
+        def open(filename)
+          gz = new(File.open(filename, 'rb'))
+
+          yield gz if block_given?
+        end
+      end
+
+      attr_reader :lineno
+
+      def initialize(io)
+        @io = io
+        @lineno = 0
+      end
+
+      def each_line(**options)
+        return to_enum(__method__, **options) unless block_given?
+
+        unused = 0
+        buf = nil
+
+        skip = options.delete(:skip_lines)
+
+        until @io.eof?
+          Zlib::GzipReader.wrap(@io) do |gz|
+            gz.each_line do |x|
+              unless x.end_with?("\n")
+                buf = x
+                next
+              end
+
+              line = buf.to_s + x
+
+              unless skip && line.match?(skip)
+                yield line
+              end
+
+              @lineno += 1
+              buf = nil
+            end
+            unused = (u = gz.unused) ? u.size : 0
+            gz.finish
+          end
+          @io.seek(-unused, IO::SEEK_CUR)
+        end
+
+        @io.close
+      end
+
+      alias each each_line
+    end
+  end
+end
