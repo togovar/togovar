f5ad850029e1897e0c930f0ff9f7dd44f84f9aa6
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-05-15 20:02:59 +0900

    initial import

---
+++ lib/togo_var/rdf/vocabulary.rb
@@ -0,0 +1,9 @@
+module TogoVar
+  module Vocabulary
+    FALDO = RDF::Vocabulary.new('http://biohackathon.org/resource/faldo#')
+    HCO = RDF::Vocabulary.new('http://identifiers.org/hco/')
+    M2R = RDF::Vocabulary.new('http://med2rdf.org/ontology/med2rdf#')
+    OBO = RDF::Vocabulary.new('http://purl.obolibrary.org/obo/')
+    TGVO = RDF::Vocabulary.new('http://togovar.biosciencedbc.jp/ontology/')
+  end
+end
