fb2c3437201018beadf32b857f1fd1621fa485ef
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-06-07 22:07:17 +0900

    remove unused codes

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,25 +1,4 @@
 module RootHelper
-  def filter_label_for(str)
-    case str.downcase
-    when 'snv', 'hgvd'
-      str.upcase
-    when 'togovar'
-      'TogoVar'
-    when 'tommo'
-      '3.5KJPN'
-    when 'exac'
-      'ExAC'
-    when 'clinvar'
-      'ClinVar'
-    when 'jga_ngs'
-      'JGA NGS'
-    when 'jga_snp'
-      'JGA SNP'
-    else
-      str.humanize
-    end
-  end
-
   def bioschemas
     <<~SCRIPT.html_safe
       <script type="application/ld+json">
