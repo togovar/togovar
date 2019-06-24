be05b6bebe2b7a43fad30f2e81cee80db84ebdc3
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-06-24 18:51:09 +0900

    update stanza parameters

--- app/controllers/reports/variant_controller.rb
+++ app/controllers/reports/variant_controller.rb
@@ -7,18 +7,19 @@ module Reports
         ep: config['triplestore']
       }.compact
 
-      search_api = config['sparqlist'].present? ? URI.parse(config['sparqlist']).merge('/') : nil
+      search_api = config['search_api'] || search_path
+      jbrowse = config['jbrowse']
 
       @header = ['variant_header', default.merge(tgv_id: id)]
 
       @stanzas = [
         ['variant_summary', nil, default.merge(tgv_id: id)],
-        ['variant_other_alternative_alleles', 'Other alternative allele(s)', default.merge(tgv_id: id, api: search_api).compact],
+        ['variant_other_alternative_alleles', 'Other alternative allele(s)', default.merge(tgv_id: id, search_api: search_api).compact],
         ['variant_frequency', 'Frequency', default.merge(tgv_id: id)],
         ['variant_clinvar', 'Clinical Significance', default.merge(tgv_id: id)],
-        ['variant_jbrowse', 'Genomic context', default.merge(tgv_id: id, assembly: 'GRCh37')],
+        ['variant_jbrowse', 'Genomic context', default.merge(tgv_id: id, assembly: 'GRCh37', jbrowse: jbrowse).compact],
         ['variant_gene', 'Gene', default.merge(tgv_id: id, assembly: 'GRCh37')],
-        ['variant_transcript', 'Transcripts', default.merge(tgv_id: id)],
+        ['variant_transcript', 'Transcripts', default.merge(tgv_id: id, assembly: 'GRCh37')],
         ['variant_publication', 'Publications', default.merge(tgv_id: id)]
       ]
     end
