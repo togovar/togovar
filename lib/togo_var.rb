2ca742b907e09d879d19d2bdc6104d2cce28c2e3
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-06-11 18:14:58 +0900

    remove unused modules

--- lib/togo_var.rb
+++ lib/togo_var.rb
@@ -1,9 +1,6 @@
 module TogoVar
   module DataSource
     module Clinvar
-      module API
-        require 'togo_var/data_source/clinvar/api/client'
-      end
       require 'togo_var/data_source/clinvar/elasticsearch_extension'
     end
 
@@ -28,18 +25,8 @@ module TogoVar
 
   module IO
     require 'togo_var/io/multi_g_zip_reader'
-    # require 'togo_var/io/ndjson'
     require 'togo_var/io/vcf'
   end
-
-  # module Models
-  #   require 'togo_var/models/condition'
-  #   require 'togo_var/models/disease'
-  #   require 'togo_var/models/frequency'
-  #   require 'togo_var/models/gene_symbol'
-  #   require 'togo_var/models/transcript'
-  #   require 'togo_var/models/variant'
-  # end
 
   module RDF
     require 'togo_var/rdf/formatter'
