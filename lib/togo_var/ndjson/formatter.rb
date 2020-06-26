40a31ef6e56cef21fa5b7bbdd0d30d1a02210909
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-06-26 11:18:36 +0900

    add require statement

--- lib/togo_var/ndjson/formatter.rb
+++ lib/togo_var/ndjson/formatter.rb
@@ -3,6 +3,7 @@ module TogoVar
     module Formatter
       require 'togo_var/ndjson/formatter/vep'
       require 'togo_var/ndjson/formatter/clin_var'
+      require 'togo_var/ndjson/formatter/frequency'
     end
   end
 end
