634f2c250f2416231af93c07a21a765e00c14924
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-20 16:14:06 +0900

    make environment variables available from frontend

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,6 @@
 const {environment} = require('@rails/webpacker')
 const webpack = require('webpack')
+const dotenv = require('dotenv')
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -18,6 +19,21 @@ environment.loaders.append('expose', {
     options: '$'
   }]
 });
+
+const dotenvFiles = [
+  'togovar.env',
+  '.env'
+]
+
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
+environment.plugins.prepend('Environment',
+  new webpack.EnvironmentPlugin(
+    JSON.parse(JSON.stringify(process.env))
+  )
+)
 
 module.exports = environment
 
