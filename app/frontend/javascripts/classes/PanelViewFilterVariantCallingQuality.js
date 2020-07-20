50acfa0000b13e2a58b1489edd979d9c8cfe2709
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-20 17:15:15 +0900

    initial import

---
+++ app/frontend/javascripts/classes/PanelViewFilterVariantCallingQuality.js
@@ -0,0 +1,24 @@
+import PanelView from "./PanelView.js";
+import StoreManager from "./StoreManager.js";
+
+export default class PanelViewFilterVariantCallingQuality extends PanelView {
+  constructor(elm) {
+    super(elm, 'quality');
+    StoreManager.bind('searchConditions', this);
+    // reference
+    this.checkbox = this.elm.querySelector('.content > label > input');
+    // event
+    this.checkbox.addEventListener('change', this.change.bind(this));
+    this.checkbox.dispatchEvent(new Event('change'));
+  }
+
+  change(e) {
+    StoreManager.setSearchCondition('quality', e.target.checked ? '1' : '0');
+  }
+
+  searchConditions(conditions) {
+    if (conditions.quality !== undefined) {
+      this.checkbox.checked = conditions.quality === '1';
+    }
+  }
+}
