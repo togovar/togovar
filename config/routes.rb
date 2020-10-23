feb025c250fcba832e94474c3209a4355a2071b0
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-10-23 16:24:04 +0900

    remove routes to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  root 'root#index'
-
-  resources :variant, only: [:show], controller: 'reports/variant', constraints: { id: /tgv\d+/ }
-
-  # static pages
-  get 'doc/about', to: 'static#about'
-  get 'doc/contact', to: 'static#contact'
-  get 'doc/datasets', to: 'static#datasets'
-  get 'doc/datasets/jga_ngs', to: 'static#jga_ngs'
-  get 'doc/datasets/jga_snp', to: 'static#jga_snp'
-  get 'doc/datasets/gem_j_wga', to: 'static#gem_j_wga'
-  get 'doc/datasets/analysis', to: 'static#analysis'
-  get 'doc/history', to: 'static#history'
-  get 'doc/help', to: 'static#help'
-  get 'doc/policy', to: 'static#policy'
-  get 'doc/terms', to: 'static#terms'
-
-  get '/downloads', to: 'static#downloads'
-
-  # api
   get 'suggest', to: 'root#suggest', defaults: { format: 'json' }
   get 'search', to: 'root#search', defaults: { format: 'json' }
 end
