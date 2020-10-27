09a2cf52cd4fe54cce70ea70c33b94635a885c8c
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-10-27 19:33:47 +0900

    do not clean up /dist on development

--- app/frontend/config/webpack.config.prod.js
+++ app/frontend/config/webpack.config.prod.js
@@ -1,6 +1,7 @@
 const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.config.common');
 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const GoogleTagManagerPlugin = require('webpack-google-tag-manager-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
@@ -16,6 +17,9 @@ if (GMT_ID) {
 
 module.exports = merge(commonConfig, {
     mode: 'production',
+    plugins: [
+      new CleanWebpackPlugin()
+    ],
     optimization: {
       minimizer: [
         new TerserJSPlugin({
