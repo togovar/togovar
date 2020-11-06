7c189e83911451b432107d6924a729945b140aca
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-11-06 20:31:43 +0900

    embed stanzas defined in config

--- app/frontend/packs/report/index.js
+++ app/frontend/packs/report/index.js
@@ -1,24 +1,44 @@
 import 'jquery'
 
+const config = require('../../config/stanza.yaml')
+
+const webcomponentsjs = config.global?.webcomponents;
+
 $(function () {
-  const id = window.location.pathname.split('/').pop();
-  const base_options = {
-    tgv_id: id,
-    ep: "https://togovar.biosciencedbc.jp/sparql",
-    sparqlist: "https://togovar.biosciencedbc.jp/sparqlist",
-  };
-  const stanza = [
-    $('<togostanza-variant_header></togostanza-variant_header>').attr(base_options),
-    $('<togostanza-variant_summary></togostanza-variant_summary>').attr(base_options),
-    $('<togostanza-variant_other_overlapping_variants></togostanza-variant_other_overlapping_variants>').attr(base_options).attr({search_api: "https://togovar.biosciencedbc.jp/search"}),
-    $('<togostanza-variant_frequency></togostanza-variant_frequency>').attr(base_options),
-    $('<togostanza-variant_clinvar></togostanza-variant_clinvar>').attr(base_options),
-    $('<togostanza-variant_jbrowse></togostanza-variant_jbrowse>').attr(base_options).attr({assembly: "GRCh37", jbrowse: "https://togovar.biosciencedbc.jp/jbrowse", margin: "50"}),
-    $('<togostanza-variant_gene></togostanza-variant_gene>').attr(base_options).attr({assembly: "GRCh37"}),
-    $('<togostanza-variant_transcript></togostanza-variant_transcript>').attr(base_options).attr({assembly: "GRCh37"}),
-    $('<togostanza-variant_publication></togostanza-variant_publication>').attr(base_options),
-  ];
+  if (webcomponentsjs) {
+    $('head').append($(`<script src="${webcomponentsjs}"></script>`));
+  }
+
+  initialize();
+});
+
+const initialize = function () {
+  const [report, id] = window.location.pathname.split('/').slice(-2);
+
+  const base_options = config[report]?.base_options || {};
+  const stanza = config[report]?.stanza || [];
+
+  base_options[config[report]?.id || 'id'] = id;
 
   $('.report_id').html(id);
-  stanza.forEach((value, index, array) => $(`#_stanza_${index}`).append(value));
-});
+
+  stanza.forEach(x => appendStanzaTag(x, base_options));
+};
+
+const appendStanzaTag = function (config, base_options) {
+  const name = config.name;
+  const dom = config.dom;
+
+  if (!name) {
+    console.error("Missing required key: 'name'");
+    return;
+  }
+
+  if (!dom) {
+    console.error("Missing required key: 'dom'");
+    return;
+  }
+
+  $('head').append($(`<link rel="import" href="${config.url}" />`))
+  $(`${dom}`).append($(`<togostanza-${name}></togostanza-${name}>`).attr(base_options || {}).attr(config.options || {}));
+};
