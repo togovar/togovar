567d8c4e0d1b1772bce620df977484a13ff336ac
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-11-30 19:52:46 +0900

    implement api call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include Loggable
+
+  def index; end
+
+  protected
+
+  # @return [Hash]
+  def request_headers
+    headers = request.headers
+    {
+      'Accept': headers['Accept'],
+      'Accept-Encoding': headers['Accept-Encoding'],
+      'Accept-Language': headers['Accept-Language'],
+      'Content-Length': headers['Content-Length'],
+      'Content-Type': headers['Content-Type'],
+      'Origin': headers['Origin'],
+      'User-Agent': headers['User-Agent']
+    }.compact
+  end
 end
