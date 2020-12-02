78bfbfe93d0c9f79afb1c1d0f6d7a9d767c3b7d7
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-12-02 14:06:55 +0900

    disable settingslogic

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,8 +1,8 @@
-require 'settingslogic'
+# require 'settingslogic'
 
-class Endpoint < Settingslogic
-  source Rails.root.join('config', 'endpoint.yml').to_s
-  namespace Rails.env
+class Endpoint # < Settingslogic
+  # source Rails.root.join('config', 'endpoint.yml').to_s
+  # namespace Rails.env
 
   def url
     @url ||= begin
