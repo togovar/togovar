90f01a0b6ef6e53e77e650ae0b962ea8fbd91e92
永野 朗夫 <nagano@me.com>
2021-01-12 17:00:55 +0900

    optimize layout function

--- app/frontend/javascripts/classes/TopPageLayoutManager.js
+++ app/frontend/javascripts/classes/TopPageLayoutManager.js
@@ -3,7 +3,8 @@ class TopPageLayoutManager {
   constructor() {
   }
 
-  init() {
+  init(targets) {
+    this.targets = targets;
     // reference
     this._GlobalHeaderHeight = document.getElementById('GlobalHeader').clientHeight;
     this._SearchInputView = document.getElementById('SearchInputView');
@@ -11,11 +12,15 @@ class TopPageLayoutManager {
     this._drawerView = document.querySelector('.drawer-view');
     
     this.update();
+    // リサイズされたらスクロール領域の更新
+    window.addEventListener('resize', this.update.bind(this));
+    window.dispatchEvent(new Event('resize'));
   }
 
   update() {
     const drawerHeight = window.innerHeight - this._drawerView.offsetTop;
     this._SearchResultsView.style.height = `calc(100vh - ${this._GlobalHeaderHeight + this._SearchInputView.clientHeight + drawerHeight}px)`;
+    this.targets.forEach(target => target.updateDisplaySize() );
   }
 
 }
