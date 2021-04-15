4243cb43f842151281f1ed4aac4cf04669aaa6e3
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2021-04-15 11:51:12 +0900

    fix offset for non-rotated items, improve animation

--- app/frontend/javascripts/classes/TippyBox.js
+++ app/frontend/javascripts/classes/TippyBox.js
@@ -1,6 +1,5 @@
 import tippy from "tippy.js";
-// All data with [data-tooltip-id] attribute will be assigned a tooltip
-// Tooltip data is stored in /assets/tooltips.json using the [data-tooltip-id] as key
+// All data with [data-tooltip-id] gets tooltip
 export default class TippyBox {
   constructor() {
     const tooltipElements = document.querySelectorAll("[data-tooltip-id]"),
@@ -14,6 +13,7 @@ export default class TippyBox {
     });
   }
 
+  // Tooltip data is stored in /assets/tooltips.json, id = [data-tooltip-id]
   getData() {
     const json = require("../../assets/tooltips.json");
     Object.freeze(json);
@@ -25,36 +25,35 @@ export default class TippyBox {
     const id = el.getAttribute("data-tooltip-id");
     try {
       const tooltip = data.find((entry) => entry.id === id),
-        template = this.createTemplate(tooltip),
-        refProps = el.getBoundingClientRect();
+        template = this.createTemplate(tooltip);
 
       tippy(el, {
         content: template.innerHTML,
         allowHTML: true,
+        animation: "fade",
+        duration: [400],
         interactive: true,
-        duration: 300,
-        theme: "brown",
+        theme: "black",
         placement: "top",
         appendTo: document.body,
         maxWidth: "15rem",
-        delay: [2, 200],
-        offset: [this.skidding(refProps), 0],
+        delay: [2, 300],
+        offset: this.offset(el),
       });
     } catch (err) {
-      // Log error if there is no tooltip info in JSON file
       console.log(
         `Failed to set the tooltip for item with a data-tooltip id of [${id}].\nCheck if there is corresponding data in tooltips.JSON`
       );
     }
   }
-  // Tippy utilizes below HTML template to set content of the tooltip
+  // HTML template for inside Tippy
   createTemplate(tooltip) {
     const template = document.createElement("span"),
       contentP = document.createElement("p");
 
     contentP.className = "content";
     contentP.innerText = tooltip.content;
-    // <a> tag will only be set when there is a URL attribute in JSON file
+    // <a> tag will only be set when there is an URL in JSON file
     if ("url" in tooltip) contentP.appendChild(this.createAnchor(tooltip.url));
     template.appendChild(contentP);
 
@@ -71,7 +70,12 @@ export default class TippyBox {
     return anchor;
   }
 
-  skidding(props) {
-    return props.width * 0.15 < 10 ? props.width * 0.1 : props.width * 0.2;
+  // Calculate offset based on props if rotated, otherwise return default
+  offset(el) {
+    const style = window.getComputedStyle(el),
+      props = el.getBoundingClientRect();
+
+    if (style.transform === "none") return [3, 7];
+    return [props.width * 0.15 < 10 ? props.width * 0.1 : props.width * 0.2, 2];
   }
 }
