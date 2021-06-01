f04f3d586670707ae74727ec2ca84f5c5dab31c4
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-06-01 15:04:23 +0900

    interpolate variables into yaml setting

--- app/frontend/packs/report/index.js
+++ app/frontend/packs/report/index.js
@@ -11,23 +11,17 @@ const STANZA_PATH = process.env.TOGOVAR_FRONTEND_STANZA_URL || '/stanza';
 
 const config = (function (obj) {
   const replace_recursive = function (obj) {
-    if (Array.isArray(obj)) {
-      for (const elem of obj) {
-        replace_recursive(elem);
+    if (typeof obj === 'string' && obj.includes('$')) {
+      for (const match of [...obj.matchAll(/(\$([A-Z_]+)|\${([A-Z_]+)})/g)]) {
+        const key = match[2] || match[3];
+        const value = ENV[key] || '';
+        obj = obj.replace(match[0], value);
       }
+    } else if (Array.isArray(obj)) {
+      obj = obj.map(x => replace_recursive(x));
     } else if (obj && typeof obj === 'object') {
       for (const [key, value] of Object.entries(obj)) {
-        if (typeof value === 'object') {
-          obj[key] = replace_recursive(value);
-        } else if (typeof value === 'string' && value.startsWith('$')) {
-          const v = ENV[value.replace(/^\$/, '')] || '';
-
-          if (v === '') {
-            delete obj[key]
-          } else {
-            obj[key] = v;
-          }
-        }
+        obj[key] = replace_recursive(value);
       }
     }
 
@@ -37,22 +31,29 @@ const config = (function (obj) {
   return replace_recursive(obj);
 })(require('../../config/stanza.yaml'));
 
-$(function () {
-  initialize();
-});
+const formatOption = function (config) {
+  const buf = {};
 
-const initialize = function () {
-  const [report, id] = window.location.pathname.split('/').slice(-2);
+  if (config) {
+    for (const [key, value] of Object.entries(config)) {
+      if (value && typeof value === 'object') {
+        buf[key] = JSON.stringify(value);
+      } else if (typeof value === 'string' && value.match(/^https?:\/\//)) {
+        const url = new URL(value);
 
-  const base_options = config[report]?.base_options || {};
-  const stanza = config[report]?.stanza || [];
+        url.search = [...url.searchParams]
+          .map(x => [x[0], encodeURIComponent(x[1])].join('='))
+          .join('&');
 
-  base_options[config[report]?.id || 'id'] = id;
+        buf[key] = url.href;
+      } else {
+        buf[key] = value;
+      }
+    }
+  }
 
-  $('.report_id').html(id);
-
-  stanza.forEach(x => appendStanzaTag(x, base_options));
-};
+  return buf;
+}
 
 const appendStanzaTag = function (config, base_options) {
   const id = config.id;
@@ -69,7 +70,35 @@ const appendStanzaTag = function (config, base_options) {
   }
 
   const src = config.src || `${STANZA_PATH}/${config.id}.js`;
+  const options = formatOption(config.options);
 
   $('head').append($(`<script type="module" src="${src}" async></script>`));
-  $(`${dom}`).append($(`<togostanza-${id}></togostanza-${id}>`).attr(base_options || {}).attr(config.options || {}));
+  $(`${dom}`).append($(`<togostanza-${id}></togostanza-${id}>`).attr(base_options || {}).attr(options || {}));
 };
+
+const initialize = function () {
+  const [report, id] = window.location.pathname.split('/').slice(-2);
+
+  const base_options = config[report]?.base_options || {};
+  const stanzas = config[report]?.stanza || [];
+
+  const id_key = config[report]?.id || 'id';
+  base_options[id_key] = id;
+
+  $('.report_id').html(id);
+
+  stanzas.forEach(stanza => {
+    if (stanza.options) {
+      for (const [key, value] of Object.entries(stanza.options)) {
+        if (typeof value === 'string' && value.includes('$')) {
+          stanza.options[key] = value.replaceAll(new RegExp(`\\$(${id_key}|{${id_key}})`, 'g'), id);
+        }
+      }
+    }
+    appendStanzaTag(stanza, base_options)
+  });
+};
+
+$(function () {
+  initialize();
+});
