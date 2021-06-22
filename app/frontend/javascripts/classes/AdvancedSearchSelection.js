094cfb4076ce8e8a82585e0d749e15d24a1ce4ce
永野 朗夫 <nagano@me.com>
2021-06-22 12:52:14 +0900

    made search condition selectable

--- app/frontend/javascripts/classes/AdvancedSearchSelection.js
+++ app/frontend/javascripts/classes/AdvancedSearchSelection.js
@@ -7,23 +7,37 @@ export default class AdvancedSearchSelection {
     this._selectionArea = new SelectionArea({
       class: 'selection-area',
       boundaries: ['#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container'],
-      selectables: ['.advanced-search-group-view', '.advanced-search-condition-view']
+      selectables: ['#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-group-view', '#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-condition-view']
     });
     console.log( this._selectionArea )
 
     this._selectionArea
-      .on('beforestart', e => {
-        console.log(e)
+      .on('beforestart', ({store, event}) => {
+        console.log(store)
       })
-      .on('start', e => {
-        console.log(e)
+      .on('start', ({store, event}) => {
+        console.log('start', store)
+        this._selectiong(store);
       })
-      .on('move', e => {
-        console.log(e)
+      .on('move', ({store, event}) => {
+        console.log('move', store)
+        this._selectiong(store);
       })
       .on('stop', ({store, event}) => {
-        console.log(store)
+        console.log('stop', store)
+        this._selectiong(store);
       });
   }
+  
+  _selectiong(store) {
+    store.changed.added.forEach(el => {
+      console.log(el.classList.contains('-editing'))
+      if (!el.classList.contains('-editing')) {
+        el.classList.add('-selected');
+      }
+    });
+    store.changed.removed.forEach(el => el.classList.remove('-selected'));
+  }
+
 
 }
