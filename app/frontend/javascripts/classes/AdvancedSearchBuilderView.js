e0967d273b0dd4e548556f022711a54bf2aae549
永野 朗夫 <nagano@me.com>
2021-06-22 15:26:36 +0900

    add grouping logic

--- app/frontend/javascripts/classes/AdvancedSearchBuilderView.js
+++ app/frontend/javascripts/classes/AdvancedSearchBuilderView.js
@@ -3,15 +3,20 @@ import ConditionGroupView from './ConditionGroupView.js';
 import AdvancedSearchToolbar from './AdvancedSearchToolbar.js';
 import AdvancedSearchSelection from './AdvancedSearchSelection.js';
 // import {ADVANCED_CONDITIONS} from '../global.js';
-import {API_URL} from "../global.js";
+// import {API_URL} from "../global.js";
 
 export default class AdvancedSearchBuilderView {
 
   constructor(elm) {
 
+    this._elm = elm;
+    console.log(elm)
     const inner = elm.querySelector(':scope > .inner');
     this._rootGroup = new ConditionGroupView(this, inner, 'and', [], true);
     this._selectingCondition = this._rootGroup;
+
+    // status
+    this._elm.dataset.selectedMultipleConditions = false;
 
     // toolbar
     this._toolbar = new AdvancedSearchToolbar(this, this._rootGroup.maketToolbar());
@@ -20,14 +25,15 @@ export default class AdvancedSearchBuilderView {
     StoreManager.bind('advancedSearchConditions', this);
 
     // select conditions
-    new AdvancedSearchSelection(this._rootGroup.elm);
+    new AdvancedSearchSelection(this._rootGroup.elm, this);
   }
 
 
   // public methods
 
-  select(condition) {
-    console.log(condition)
+  select(elements) {
+    console.log(elements)
+    this._elm.dataset.selectedMultipleConditions = elements.length > 1;
   }
 
   changeCondition() {
