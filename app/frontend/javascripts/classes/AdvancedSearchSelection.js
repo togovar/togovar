859ff81943a4221434ca8182a41c89ba585b263f
永野 朗夫 <nagano@me.com>
2021-06-22 14:27:09 +0900

    modify selecting logic

--- app/frontend/javascripts/classes/AdvancedSearchSelection.js
+++ app/frontend/javascripts/classes/AdvancedSearchSelection.js
@@ -3,29 +3,48 @@ import SelectionArea from '@simonwep/selection-js';
 export default class AdvancedSearchSelection {
 
   constructor(area) {
+    console.log( area )
     console.log( SelectionArea )
     this._selectionArea = new SelectionArea({
       class: 'selection-area',
       boundaries: ['#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container'],
-      selectables: ['#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-group-view', '#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-condition-view']
+      selectables: ['#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-group-view', '#AdvancedSearchBuilderView > .inner > .advanced-search-group-view.-root > .container .advanced-search-condition-view'],
+      startareas: ['html'],
+      overlap: 'invert',
+      singleTap: {
+        allow: false
+      }
     });
     console.log( this._selectionArea )
 
     this._selectionArea
-      .on('beforestart', ({store, event}) => {
-        console.log(store)
-      })
+      // .on('beforestart', ({store, event}) => {
+      //   console.log(store)
+      // })
       .on('start', ({store, event}) => {
-        console.log('start', store)
-        this._selectiong(store);
+        if (!event.ctrlKey && !event.metaKey) {
+          // Unselect all elements
+          for (const el of store.stored) {
+              el.classList.remove('-selected');
+          }
+          // Clear previous selection
+          this._selectionArea.clearSelection();
+        }
       })
-      .on('move', ({store, event}) => {
-        console.log('move', store)
-        this._selectiong(store);
+      .on('move', ({store: {changed: {added, removed}}}) => {
+        // Add a custom class to the elements that where selected.
+        for (const el of added) {
+          el.classList.add('-selected');
+        }
+        // Remove the class from elements that where removed
+        // since the last selection
+        for (const el of removed) {
+          el.classList.remove('-selected');
+        }
       })
-      .on('stop', ({store, event}) => {
-        console.log('stop', store)
-        this._selectiong(store);
+      .on('stop', (e) => {
+        console.log(e)
+        this._selectionArea.keepSelection();
       });
   }
   
