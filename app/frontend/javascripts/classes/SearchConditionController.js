14adb5dc81eb2150524e0b4a0a6e55886e89f42f
永野 朗夫 <nagano@me.com>
2021-09-01 09:14:08 +0900

    change search field generative

--- app/frontend/javascripts/classes/SearchConditionController.js
+++ app/frontend/javascripts/classes/SearchConditionController.js
@@ -2,6 +2,32 @@ import StoreManager from "./StoreManager.js";
 import SearchFieldView from "./SearchFieldView.js";
 // import {API_URL} from "../global.js";
 
+const EXAMPLES = [
+  {
+    key: 'Disease',
+    value: 'Breast-ovarian cancer, familial 2'
+  },
+  {
+    key: 'Gene',
+    value: 'ALDH2'
+  },
+  {
+    key: 'refSNP',
+    value: 'rs114202595'
+  },
+  {
+    key: 'TogoVar',
+    value: 'tgv421843'
+  },
+  {
+    key: 'Position(GRCh37/hg19)',
+    value: '16:48258198'
+  },
+  {
+    key: 'Region(GRCh37/hg19)',
+    value: '10:73270743-73376976'
+  }
+]
 
 export default class SearchConditionController {
 
@@ -11,20 +37,25 @@ export default class SearchConditionController {
     this._searchFieldView = new SearchFieldView(
       this,
       elm,
+      'Search for disease or gene symbol or rs...',
       ['gene', 'disease']
     );
 
     // events
     StoreManager.bind('searchConditions', this);
-    elm.querySelectorAll('.searchexamples').forEach(dl => {
-      dl.addEventListener('click', e => {
-        e.stopPropagation();
-        this._searchFieldView.setTerm(dl.querySelector('dd').textContent, true);
-      }, true);
-    });
     // value
     const term = StoreManager.getSearchCondition('term');
     if (term) this._searchFieldView.setTerm(term);
+
+    // examples
+    this._searchFieldView.setExamples(EXAMPLES)
+      .forEach(dl => {
+        dl.addEventListener('click', e => {
+          e.stopPropagation();
+          this._searchFieldView.setTerm(dl.querySelector('dd').textContent, true);
+        }, true);
+      });
+
   }
 
   search(value) {
