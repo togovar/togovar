0049de9447e0409e008afda0080b1ac6dc3d6372
永野 朗夫 <nagano@me.com>
2021-09-04 20:30:47 +0900

    add some parts in freq editor

--- app/frontend/javascripts/classes/ConditionValueEditorFrequencyCount.js
+++ app/frontend/javascripts/classes/ConditionValueEditorFrequencyCount.js
@@ -1,5 +1,7 @@
-import SearchFieldView from './SearchFieldView.js';
+import RangeSelectorView from "./RangeSelectorView.js";
 import {CONDITION_TYPE} from '../definition.js';
+
+let id = 0;
 
 export default class ConditionValueEditorFrequencyCount {
 
@@ -8,19 +10,46 @@ export default class ConditionValueEditorFrequencyCount {
 
     this._valuesView = valuesView;
     this._conditionType = conditionType;
+    const name = `ConditionValueEditorFrequencyCount${id++}`;
 
     // HTML
     const section = document.createElement('section');
     section.classList.add('text-field-editor-view');
     section.innerHTML = `
       <header>Select ${conditionType}</header>
-      <div class="body"></div>`;
+      <div class="body">
+        <section class="frequency">
+          <label>
+            <input type="radio" name="${name}">
+            <span>Frequency<span>
+          </label>
+          <div class="range-selector-view"></div>
+        </section>
+        <section class="count">
+          <label>
+            <input type="radio" name="${name}">
+            <span>Count<span>
+          </label>
+        </section>
+      </div>`;
     valuesView.sections.append(section);
     this._body = section.querySelector(':scope > .body');
+
+    const rangeSelectorView = section.querySelector('.range-selector-view');
+    this._rangeSelectorViews = new RangeSelectorView(rangeSelectorView, this, 0, 1, 'horizontal', 'advanced');
+    console.log(this._rangeSelectorViews)
+
+
   }
 
 
   // public methods
+
+  changeParameter(newCondition, dataset) {
+    console.log(newCondition, dataset)
+    console.log(this._rangeSelectorViews)
+    this._rangeSelectorViews.updateGUIWithCondition(newCondition);
+  }
 
   keepLastValues() {
     // this._lastValue = this._searchFieldView.value;
