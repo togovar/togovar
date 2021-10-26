7c67b4dba063ed2d6489e50e858ac76633081ccb
永野 朗夫 <nagano@me.com>
2021-10-26 09:12:56 +0900

    change delimiter of chromosome position view

--- app/frontend/javascripts/classes/ChromosomePositionView.js
+++ app/frontend/javascripts/classes/ChromosomePositionView.js
@@ -3,7 +3,7 @@ export default class ChromosomePositionView {
   constructor(container) {
     container.innerHTML = `
     <span class="chromosome-position">
-      <span class="chromosome"></span> <span class="coordinate"></span>
+      <span class="chromosome"></span><span class="taking">-</span><span class="coordinate"></span>
     </span>`;
     const root = container.querySelector(':scope > .chromosome-position');
     this._chr = root.querySelector('.chromosome');
