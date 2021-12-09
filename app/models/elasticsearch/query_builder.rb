3e537014a5e609fef521c973186cf327f55d2c7c
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-12-09 17:11:29 +0900

    bug fix

--- app/models/elasticsearch/query_builder.rb
+++ app/models/elasticsearch/query_builder.rb
@@ -567,10 +567,10 @@ module Elasticsearch
             query do
               bool do
                 must do
-                  match 'vep.symbol.label': 'HGNC' # FIXME
+                  match 'vep.symbol.label': [term]
                 end
                 must do
-                  terms 'vep.symbol.source': [term] # FIXME
+                  terms 'vep.symbol.source': 'HGNC'
                 end
               end
             end
