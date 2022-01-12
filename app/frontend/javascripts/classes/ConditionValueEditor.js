72c0a9a18eda38551a3c9c1625613a8e89df3dc7
永野 朗夫 <nagano@me.com>
2022-01-12 14:16:28 +0900

    transfer creating element section from classes to super class, and fix column editor issue

--- app/frontend/javascripts/classes/ConditionValueEditor.js
+++ app/frontend/javascripts/classes/ConditionValueEditor.js
@@ -7,4 +7,13 @@ export default class ConditionValueEditor {
 
   }
 
+  _createElement(className, html) {
+    this._el = document.createElement('section');
+    this._el.classList.add(className);
+    this._el.dataset.conditionType = this._conditionType;
+    this._el.innerHTML = html;
+    this._valuesView.sections.append(this._el);
+    this._body = this._el.querySelector(':scope > .body');
+  }
+
 }
