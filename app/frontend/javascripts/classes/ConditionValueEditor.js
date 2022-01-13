075d8ea795a0667219113d5587b8f83df0a63fc8
永野 朗夫 <nagano@me.com>
2022-01-13 18:43:32 +0900

    add remove logic in condition value editor, and apply checkbox editor

--- app/frontend/javascripts/classes/ConditionValueEditor.js
+++ app/frontend/javascripts/classes/ConditionValueEditor.js
@@ -40,15 +40,19 @@ export default class ConditionValueEditor {
       valueView = document.createElement('span');
       valueView.classList.add('value', 'condition-item-value-view');
       valueView.dataset.value = value;
+      valueView.dataset.conditionType = this._conditionType;
       valueView.innerHTML = `<span class="inner">${label}</span>`;
       this._valuesEl.append(valueView);
     }
     return valueView;
   }
 
-  // _enterValueView(value, isExist) {
-
-  // }
+  _removeValueView(value) {
+    const valueView = this._valuesEl.querySelector(`.value[data-value="${value}"]`);
+    if (valueView) {
+      valueView.remove();
+    }
+  }
 
   get _valuesEl() {
     return this._valuesView.conditionView.valuesElement;
