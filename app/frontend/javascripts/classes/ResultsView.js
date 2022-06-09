7b7763f45d9b9b1715f2205eb5e05af56c86d229
Anton <nokker@gmail.com>
2022-06-09 12:31:08 +0900

    bigger number of results font

--- app/frontend/javascripts/classes/ResultsView.js
+++ app/frontend/javascripts/classes/ResultsView.js
@@ -1,53 +1,71 @@
 import StoreManager from "./StoreManager.js";
 import ResultsRowView from "./ResultsRowView.js";
 import ScrollBar from "./ScrollBar.js";
-import { TR_HEIGHT, COMMON_FOOTER_HEIGHT, COLUMNS } from '../global.js';
-
+import { TR_HEIGHT, COMMON_FOOTER_HEIGHT, COLUMNS } from "../global.js";
 
 export default class ResultsView {
-
   constructor(elm) {
     this.elm = elm;
     this.rows = [];
     this.lastScroll = 0;
-    this.status = this.elm.querySelector('header.header > .status');
-    this.messages = this.elm.querySelector('#Messages');
+    this.status = this.elm.querySelector("header.header > .status");
+    this.messages = this.elm.querySelector("#Messages");
 
-    StoreManager.bind('searchStatus', this);
-    StoreManager.bind('searchResults', this);
-    StoreManager.bind('columns', this);
-    StoreManager.bind('offset', this);
-    StoreManager.bind('karyotype', this);
-    StoreManager.bind('searchMessages', this);
-    $(document).on('keydown.resultview', this.keydown.bind(this));
+    StoreManager.bind("searchStatus", this);
+    StoreManager.bind("searchResults", this);
+    StoreManager.bind("columns", this);
+    StoreManager.bind("offset", this);
+    StoreManager.bind("karyotype", this);
+    StoreManager.bind("searchMessages", this);
+    $(document).on("keydown.resultview", this.keydown.bind(this));
     // スクロールバーの生成
-    this.elm.querySelector('.tablecontainer').insertAdjacentHTML('afterend', '<div class="scroll-bar"></div>');
-    new ScrollBar(this.elm.querySelector('.scroll-bar'));
+    this.elm
+      .querySelector(".tablecontainer")
+      .insertAdjacentHTML("afterend", '<div class="scroll-bar"></div>');
+    new ScrollBar(this.elm.querySelector(".scroll-bar"));
     // ヘッダ+ ヘッダのツールチップ用のデータ設定
-    this.elm.querySelector('.tablecontainer > table.results-view > thead > tr').innerHTML = COLUMNS.map(column => `<th class="${column.id}"><p data-tooltip-id="table-header-${column.id}">${column.label}</p></th>`).join('');
+    this.elm.querySelector(
+      ".tablecontainer > table.results-view > thead > tr"
+    ).innerHTML = COLUMNS.map(
+      (column) =>
+        `<th class="${column.id}"><p data-tooltip-id="table-header-${column.id}">${column.label}</p></th>`
+    ).join("");
 
-    this.tbody = this.elm.querySelector('.tablecontainer > table.results-view > tbody');
+    this.tbody = this.elm.querySelector(
+      ".tablecontainer > table.results-view > tbody"
+    );
 
     // スクロール制御
     // スクロールイベント
-    const mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    const mousewheelevent =
+      "onwheel" in document
+        ? "wheel"
+        : "onmousewheel" in document
+        ? "mousewheel"
+        : "DOMMouseScroll";
     this.tbody.addEventListener(mousewheelevent, this.scroll.bind(this));
 
     // カラムの表示を制御するためのスタイルシート
-    this.stylesheet = document.createElement('style');
-    this.stylesheet.type = 'text/css';
-    document.getElementsByTagName('head')[0].appendChild(this.stylesheet);
-    this.columns(StoreManager.getData('columns'));
+    this.stylesheet = document.createElement("style");
+    this.stylesheet.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(this.stylesheet);
+    this.columns(StoreManager.getData("columns"));
   }
 
   updateDisplaySize() {
     // 表示数
-    const
-      maxRowCount = Math.floor((window.innerHeight - this.tbody.getBoundingClientRect().top - StoreManager.getData('karyotype').height - COMMON_FOOTER_HEIGHT - 2) / TR_HEIGHT),
-      numberOfRecords = StoreManager.getData('numberOfRecords'),
-      offset = StoreManager.getData('offset'),
+    const maxRowCount = Math.floor(
+        (window.innerHeight -
+          this.tbody.getBoundingClientRect().top -
+          StoreManager.getData("karyotype").height -
+          COMMON_FOOTER_HEIGHT -
+          2) /
+          TR_HEIGHT
+      ),
+      numberOfRecords = StoreManager.getData("numberOfRecords"),
+      offset = StoreManager.getData("offset"),
       rowCount = Math.min(maxRowCount, numberOfRecords);
-    StoreManager.setData('rowCount', rowCount);
+    StoreManager.setData("rowCount", rowCount);
     // 行が足らなければ追加
     if (this.rows.length < rowCount) {
       for (let i = this.rows.length; i < rowCount; i++) {
@@ -57,24 +75,25 @@ export default class ResultsView {
       }
     }
     // オフセット量の調整
-    const
-      onScreen = numberOfRecords - offset,
+    const onScreen = numberOfRecords - offset,
       belowSpace = maxRowCount - onScreen;
-    if (belowSpace > 0) { // 隙間ができてしまい
-      if (offset >= belowSpace) { // 上の隙間の方が大きい場合、差分をオフセットにセット
-        StoreManager.setData('offset', offset - belowSpace);
-      } else { // 下の隙間が大きい場合、オフセット量をゼロに
-        StoreManager.setData('offset', 0);
+    if (belowSpace > 0) {
+      // 隙間ができてしまい
+      if (offset >= belowSpace) {
+        // 上の隙間の方が大きい場合、差分をオフセットにセット
+        StoreManager.setData("offset", offset - belowSpace);
+      } else {
+        // 下の隙間が大きい場合、オフセット量をゼロに
+        StoreManager.setData("offset", 0);
       }
     }
   }
 
   scroll(e) {
     e.stopPropagation();
-    const
-      totalHeight = StoreManager.getData('numberOfRecords') * TR_HEIGHT;
-    let
-      availableScrollY = totalHeight - StoreManager.getData('rowCount') * TR_HEIGHT,
+    const totalHeight = StoreManager.getData("numberOfRecords") * TR_HEIGHT;
+    let availableScrollY =
+        totalHeight - StoreManager.getData("rowCount") * TR_HEIGHT,
       wheelScroll;
     availableScrollY = availableScrollY < 0 ? 0 : availableScrollY;
     // 縦方向にスクロールしていない場合スルー
@@ -82,20 +101,22 @@ export default class ResultsView {
     // ホイールのスクロール量
     wheelScroll = this.lastScroll + e.deltaY;
     wheelScroll = wheelScroll < 0 ? 0 : wheelScroll;
-    wheelScroll = wheelScroll > availableScrollY ? availableScrollY : wheelScroll;
+    wheelScroll =
+      wheelScroll > availableScrollY ? availableScrollY : wheelScroll;
     if (wheelScroll === this.lastScroll) return;
     // スクロール量決定
     this.lastScroll = wheelScroll;
     // 表示行位置
     let offset = Math.ceil(this.lastScroll / TR_HEIGHT);
-    StoreManager.setData('offset', offset);
+    StoreManager.setData("offset", offset);
   }
 
   offset(offset) {
     this.lastScroll = offset * TR_HEIGHT;
     // 染色体位置
-    const displayingRegions1 = {}, displayingRegions2 = {};
-    for (let i = 0; i <= StoreManager.getData('rowCount') - 1; i++) {
+    const displayingRegions1 = {},
+      displayingRegions2 = {};
+    for (let i = 0; i <= StoreManager.getData("rowCount") - 1; i++) {
       const record = StoreManager.getRecordByIndex(i);
       if (displayingRegions1[record.chromosome] === undefined) {
         displayingRegions1[record.chromosome] = [];
@@ -105,14 +126,14 @@ export default class ResultsView {
     for (const key in displayingRegions1) {
       displayingRegions2[key] = {
         start: Math.min(...displayingRegions1[key]),
-        end: Math.max(...displayingRegions1[key])
-      }
+        end: Math.max(...displayingRegions1[key]),
+      };
     }
-    StoreManager.setData('displayingRegionsOnChromosome', displayingRegions2);
+    StoreManager.setData("displayingRegionsOnChromosome", displayingRegions2);
   }
 
   searchMessages(messages) {
-    this.messages.innerHTML = '';
+    this.messages.innerHTML = "";
 
     if (messages.notice) {
       this.messages.innerHTML += `<div class="message -notice">${messages.notice}</div>`;
@@ -125,15 +146,15 @@ export default class ResultsView {
     }
   }
 
-  
   // bindings ///////////////////////////
 
   searchStatus(status) {
-    this.status.textContent = `The number of available variations is ${status.available.toLocaleString()} out of ${status.filtered.toLocaleString()}.`;
+    this.status.innerHTML = `The number of available variations is ${status.available.toLocaleString()} out of <span class="bigger">${status.filtered.toLocaleString()}</span>.`;
+    // this.status.textContent = `The number of available variations is ${status.available.toLocaleString()} out of ${status.filtered.toLocaleString()}.`;
     if (status.filtered === 0) {
-      this.elm.classList.add('-not-found');
+      this.elm.classList.add("-not-found");
     } else {
-      this.elm.classList.remove('-not-found');
+      this.elm.classList.remove("-not-found");
     }
   }
 
@@ -150,56 +171,59 @@ export default class ResultsView {
     // スタイルの追加
     for (let i = 0; i < columns.length; i++) {
       const column = columns[i];
-      this.stylesheet.sheet.insertRule(`
-      .tablecontainer > table.results-view th.${column.id}, .tablecontainer > table.results-view td.${column.id} {
-        display: ${ column.isUsed ? 'table-cell' : 'none' }
-      }`, i);
+      this.stylesheet.sheet.insertRule(
+        `
+      .tablecontainer > table.results-view th.${
+        column.id
+      }, .tablecontainer > table.results-view td.${column.id} {
+        display: ${column.isUsed ? "table-cell" : "none"}
+      }`,
+        i
+      );
     }
   }
 
   // 上下カーソルタイプで選択行の移動 & ESCで選択解除
   keydown(e) {
-    if (StoreManager.getData('selectedRow') === undefined) return;
+    if (StoreManager.getData("selectedRow") === undefined) return;
     switch (e.key) {
-      case 'ArrowUp': // ↑
+      case "ArrowUp": // ↑
         this.shiftSelectedRow(-1);
         break;
-      case 'ArrowDown': // ↓
+      case "ArrowDown": // ↓
         this.shiftSelectedRow(1);
         break;
-      case 'Escape': // 選択解除
-        StoreManager.setData('selectedRow', undefined);
+      case "Escape": // 選択解除
+        StoreManager.setData("selectedRow", undefined);
         break;
     }
   }
 
   shiftSelectedRow(value) {
-    let
-      currentIndex = StoreManager.getData('selectedRow'),
+    let currentIndex = StoreManager.getData("selectedRow"),
       shiftIndex = currentIndex + value,
-      rowCount = StoreManager.getData('rowCount'),
-      offset = StoreManager.getData('offset'),
-      numberOfRecords = StoreManager.getData('numberOfRecords');
+      rowCount = StoreManager.getData("rowCount"),
+      offset = StoreManager.getData("offset"),
+      numberOfRecords = StoreManager.getData("numberOfRecords");
     if (shiftIndex < 0) {
-      shiftIndex = 0
+      shiftIndex = 0;
       if (offset > 0) {
         // 上にスクロール
         offset--;
-        StoreManager.setData('offset', offset);
+        StoreManager.setData("offset", offset);
       }
-    } else if (shiftIndex > (rowCount - 1)) {
+    } else if (shiftIndex > rowCount - 1) {
       shiftIndex = rowCount - 1;
-      if ((offset + shiftIndex) < (numberOfRecords - 1)) {
+      if (offset + shiftIndex < numberOfRecords - 1) {
         // 下にスクロール
         offset++;
-        StoreManager.setData('offset', offset);
+        StoreManager.setData("offset", offset);
       }
     }
-    StoreManager.setData('selectedRow', shiftIndex);
+    StoreManager.setData("selectedRow", shiftIndex);
   }
 
   karyotype(_karyotype) {
     this.updateDisplaySize();
   }
-
 }
