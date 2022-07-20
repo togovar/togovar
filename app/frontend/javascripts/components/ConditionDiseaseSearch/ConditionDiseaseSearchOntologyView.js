5f854fe65b01ac531e79ca69155e7e76ee0804ef
Anton <nokker@gmail.com>
2022-07-20 16:07:43 +0900

    add flip directive, no errors

--- app/frontend/javascripts/components/ConditionDiseaseSearch/ConditionDiseaseSearchOntologyView.js
+++ app/frontend/javascripts/components/ConditionDiseaseSearch/ConditionDiseaseSearchOntologyView.js
@@ -1,38 +1,41 @@
 import { LitElement, css, html } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
+
+import { flip } from './flip';
+
 import './OntologyCard';
-import { directive } from 'lit-html';
 
 const DISEASE_ADVANCED_SEARCH_URL = `https://togovar-stg.biosciencedbc.jp/api/inspect/disease?node=`;
 
-const flip = directive((options = { duration: 300 }, onfinish) => (part) => {
-  const firstElement = part.committer.element;
-  // Don't animate first render
-  if (!firstElement.isConnected) {
-    return;
-  }
-  // Capture render position before update
-  const first = firstElement.getBoundingClientRect();
-  // Nodes may be re-used so identify via a key.
-  const container = firstElement.parentNode;
-  const key = firstElement.getAttribute('key');
-  requestAnimationFrame(() => {
-    // Find matching element.
-    const lastElement = container.querySelector(`[key="${key}"]`);
-    if (!lastElement) {
-      return;
-    }
-    // Capture render position after update
-    const last = lastElement.getBoundingClientRect();
-    // Calculate deltas and animate if something changed.
-    const topChange = first.top - last.top;
-    if (topChange !== 0) {
-      lastElement.animate(
-        [{ transform: `translate(${topChange}px, ${topChange}px)` }, {}],
-        options
-      ).onfinish = onfinish;
-    }
-  });
-});
+// const flip = directive((options = { duration: 300 }, onfinish) => (part) => {
+//   const firstElement = part.committer.element;
+//   // Don't animate first render
+//   if (!firstElement.isConnected) {
+//     return;
+//   }
+//   // Capture render position before update
+//   const first = firstElement.getBoundingClientRect();
+//   // Nodes may be re-used so identify via a key.
+//   const container = firstElement.parentNode;
+//   const key = firstElement.getAttribute('key');
+//   requestAnimationFrame(() => {
+//     // Find matching element.
+//     const lastElement = container.querySelector(`[key="${key}"]`);
+//     if (!lastElement) {
+//       return;
+//     }
+//     // Capture render position after update
+//     const last = lastElement.getBoundingClientRect();
+//     // Calculate deltas and animate if something changed.
+//     const topChange = first.top - last.top;
+//     if (topChange !== 0) {
+//       lastElement.animate(
+//         [{ transform: `translate(${topChange}px, ${topChange}px)` }, {}],
+//         options
+//       ).onfinish = onfinish;
+//     }
+//   });
+// });
 
 export default class CondDiseaseOntologyView extends LitElement {
   static get properties() {
@@ -45,10 +48,6 @@ export default class CondDiseaseOntologyView extends LitElement {
       data: {
         type: Object,
         state: true,
-        hasChanged(newVal, oldVal) {
-          console.log('hasChanged', oldVal, newVal);
-          this.children = newVal.children;
-        },
       },
       children: {
         type: Array,
@@ -146,16 +145,38 @@ export default class CondDiseaseOntologyView extends LitElement {
 
   duration = 1000;
 
-  // rotate() {
-  //   // async/debounce the animation
-  //   if (!this._scheduled) {
-  //     this._scheduled = setTimeout(() => {
-  //       this._scheduled = undefined;
-  //       shuffleArray(this.items);
-  //       this.requestUpdate();
-  //     }, this.duration);
-  //   }
-  // }
+  flip(firstStyleMap, lastStyleMap, listener, removing) {
+    console.log('this', this.getBoundingClientRect());
+    const previous = this.boundingRect;
+    this.boundingRect = this.getBoundingClientRect();
+    const deltaX = previous.x - this.boundingRect.x;
+    const deltaY = previous.y - this.boundingRect.y;
+    if (!deltaX && !deltaY && !removing) {
+      return;
+    }
+    const filteredListener = (event) => {
+      if (event.target === this) {
+        listener(event);
+        this.removeEventListener('transitionend', filteredListener);
+      }
+    };
+    this.addEventListener('transitionend', filteredListener);
+    const translate = `translate(${deltaX}px, ${deltaY}px)`;
+    this.applyStyles({
+      ...firstStyleMap,
+      transform: `${translate} ${firstStyleMap.transform ?? ''}`,
+    });
+    requestAnimationFrame(() => {
+      const transition = `transform ${this.options.duration}ms ${this.options.timingFunction} ${this.options.delay}ms`;
+      this.applyStyles({
+        ...lastStyleMap,
+        transition,
+        transform: `${removing ? `${translate} ` : ''}${
+          lastStyleMap.transform ?? ''
+        }`,
+      });
+    });
+  }
 
   render() {
     return html`
@@ -166,17 +187,16 @@ export default class CondDiseaseOntologyView extends LitElement {
             (this.error && html`<div>Error</div>`)
           : html`<div class="search-field-view-content">
               <div class="cards-container">
-                ${this.data.parents.map(
-                  (parent) =>
-                    html`
-                      <ontology-card
-                        key="${parent.id}"
-                        ...=${flip({ duration: this.duration }, () => {})}
-                        id="${parent.id}"
-                        .data=${parent}
-                        @card_selected=${(e) => this._fetchData(e.detail.id)}
-                      />
-                    `
+                ${repeat(
+                  this.data.parents,
+                  (i) => i.id,
+                  (i) =>
+                    html`<ontology-card
+                      key="${i.id}"
+                      ${flip()}
+                      .data=${i}
+                      @card_selected=${(e) => this._fetchData(e.detail.id)}
+                    />`
                 )}
               </div>
               <div class="cards-container main">
@@ -192,9 +212,6 @@ export default class CondDiseaseOntologyView extends LitElement {
                     html`
                       <ontology-card
                         key="${parent.id}"
-                        ...=${flip({ duration: this.duration }, () =>
-                          this.rotate()
-                        )}
                         .data=${child}
                         id="${child.id}"
                         @card_selected=${(e) => this._fetchData(e.detail.id)}
