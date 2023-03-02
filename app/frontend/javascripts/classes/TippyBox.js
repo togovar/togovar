b29ac25abb25a23f0e165ba40d894779af2fc42a
Yukiko Noda <yukiko.noda@penqe.com>
2023-03-02 10:29:07 +0900

    fix z-index

--- app/frontend/javascripts/classes/TippyBox.js
+++ app/frontend/javascripts/classes/TippyBox.js
@@ -1,13 +1,13 @@
-import tippy from "tippy.js";
+import tippy from 'tippy.js';
 // All data with [data-tooltip-id] gets tooltip
 export default class TippyBox {
   constructor() {
-    const tooltipElements = document.querySelectorAll("[data-tooltip-id]"),
+    const tooltipElements = document.querySelectorAll('[data-tooltip-id]'),
       data = this.getData();
 
     tooltipElements.forEach((HTMLElement) => {
       HTMLElement.addEventListener(
-        "mouseover",
+        'mouseover',
         this.setTooltip(HTMLElement, data)
       );
     });
@@ -15,14 +15,15 @@ export default class TippyBox {
 
   // Tooltip data is stored in /assets/tooltips.json, id = [data-tooltip-id]
   getData() {
-    const json = require("../../assets/tooltips.json");
+    const json = require('../../assets/tooltips.json');
     Object.freeze(json);
-    
+
     return json;
   }
   // Set tooltip with tippy.js plugin
   setTooltip(el, data) {
-    const id = el.getAttribute("data-tooltip-id");
+    const id = el.getAttribute('data-tooltip-id');
+
     try {
       const tooltip = data.find((entry) => entry.id === id),
         template = this.createTemplate(tooltip);
@@ -30,15 +31,16 @@ export default class TippyBox {
       tippy(el, {
         content: template.innerHTML,
         allowHTML: true,
-        animation: "fade",
+        animation: 'fade',
         duration: [400],
         interactive: true,
-        theme: "black",
-        placement: "top",
+        theme: 'black',
+        placement: 'top',
         appendTo: document.body,
-        maxWidth: "15rem",
+        maxWidth: '15rem',
         delay: [2, 300],
         offset: this.offset(el),
+        zIndex: 10100,
       });
     } catch (err) {
       console.error(
@@ -48,24 +50,24 @@ export default class TippyBox {
   }
   // HTML template for inside Tippy
   createTemplate(tooltip) {
-    const template = document.createElement("span"),
-      contentP = document.createElement("p");
+    const template = document.createElement('span'),
+      contentP = document.createElement('p');
 
-    contentP.className = "content";
+    contentP.className = 'content';
     contentP.innerText = tooltip.content;
     // <a> tag will only be set when there is an URL in JSON file
-    if ("url" in tooltip) contentP.appendChild(this.createAnchor(tooltip.url));
+    if ('url' in tooltip) contentP.appendChild(this.createAnchor(tooltip.url));
     template.appendChild(contentP);
 
     return template;
   }
 
   createAnchor(url) {
-    const anchor = document.createElement("a");
+    const anchor = document.createElement('a');
 
-    anchor.className = "url";
+    anchor.className = 'url';
     anchor.href = url;
-    anchor.innerText = "Read More";
+    anchor.innerText = 'Read More';
 
     return anchor;
   }
@@ -75,7 +77,7 @@ export default class TippyBox {
     const style = window.getComputedStyle(el),
       props = el.getBoundingClientRect();
 
-    if (style.transform === "none") return [3, 7];
+    if (style.transform === 'none') return [3, 7];
     return [props.width * 0.15 < 10 ? props.width * 0.1 : props.width * 0.2, 2];
   }
 }
