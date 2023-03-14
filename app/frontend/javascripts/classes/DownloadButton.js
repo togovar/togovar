d785d264cfef64cec002edce654be4d20a121e1b
Yukiko Noda <yukiko8318@gmail.com>
2023-03-14 22:20:53 +0900

    fix classname & selector name & private class

--- app/frontend/javascripts/classes/DownloadButton.js
+++ app/frontend/javascripts/classes/DownloadButton.js
@@ -1,29 +1,31 @@
 import StoreManager from './StoreManager.js';
 import { API_URL } from '../global.js';
 
-export default class Download {
+export default class DownloadButton {
+  #trigger;
+  #filetype;
+  #path;
+  #options;
+
   constructor(trigger, accept, filetype) {
-    this.trigger = trigger;
-    this.filetype = filetype;
-    this.path = `${API_URL}/api/download/variant`;
-    this.options = {
+    this.#trigger = trigger;
+    this.#filetype = filetype;
+    this.#path = `${API_URL}/api/download/variant`;
+    this.#options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Accept: accept,
       },
-      body: { query: {} },
       mode: 'cors',
     };
 
-    this._switchDisplayWithTabs();
-    this.trigger.addEventListener('click', this._downloadFile.bind(this));
+    this.#switchDisplayWithTabs();
+    this.#trigger.addEventListener('click', this.#downloadFile.bind(this));
   }
 
   static switchDisplayWithConditions() {
-    const buttonGroupEl = document.querySelectorAll(
-      '.right-header > li > button'
-    );
+    const buttonGroupEl = document.querySelectorAll('.right > li > button');
     const conditions = StoreManager._store.advancedSearchConditions;
     const hasConditions = Object.keys(conditions).length > 0;
     buttonGroupEl.forEach((button) => {
@@ -31,35 +33,36 @@ export default class Download {
     });
   }
 
-  _switchDisplayWithTabs() {
+  #switchDisplayWithTabs() {
     const tabs = document.querySelectorAll('[data-tab-group]');
     tabs.forEach((tab) => {
       tab.addEventListener('click', () => {
         const selectTab = tab.getAttribute('data-target');
-        const downloadGroupEl = document.querySelector('.right-header');
+        const downloadGroupEl = document.querySelector('.right');
         downloadGroupEl.style.display = selectTab === 'simple' ? 'none' : '';
       });
     });
   }
 
-  _downloadQuery() {
+  #downloadQuery() {
+    this.#options.body = { query: {} };
     if (StoreManager._URIParameters.mode === 'advanced') {
-      this.options.body.query = StoreManager._store.advancedSearchConditions;
-      this.options.body = JSON.stringify(this.options.body);
+      this.#options.body.query = StoreManager._store.advancedSearchConditions;
+      this.#options.body = JSON.stringify(this.#options.body);
     }
   }
 
-  async _downloadFile() {
-    this._downloadQuery();
+  async #downloadFile() {
+    this.#downloadQuery();
     try {
-      const response = await fetch(this.path, this.options, this.filetype);
+      const response = await fetch(this.#path, this.#options, this.#filetype);
       if (!response.ok) {
         throw new Error('Failed to download file');
       }
       const blobUrl = URL.createObjectURL(await response.blob());
       const link = document.createElement('a');
       link.href = blobUrl;
-      link.download = `togovar.${this.filetype}`;
+      link.download = `togovar.${this.#filetype}`;
       link.click();
       URL.revokeObjectURL(blobUrl);
     } catch (error) {
