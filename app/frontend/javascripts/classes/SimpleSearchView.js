615f8342c2d13722bb59361f82c8f390b1e0c899
Yukiko Noda <yukiko8318@gmail.com>
2023-04-17 10:52:05 +0900

    revert change

--- app/frontend/javascripts/classes/SimpleSearchView.js
+++ app/frontend/javascripts/classes/SimpleSearchView.js
@@ -1,5 +1,6 @@
 import StoreManager from './StoreManager.js';
 import SearchFieldView from './SearchFieldView.js';
+import { API_URL } from '../global.js';
 
 const EXAMPLES = (() => {
   switch (TOGOVAR_FRONTEND_REFERENCE) {
@@ -7,71 +8,71 @@ const EXAMPLES = (() => {
       return [
         {
           key: 'Disease',
-          value: 'Breast-ovarian cancer, familial 2'
+          value: 'Breast-ovarian cancer, familial 2',
         },
         {
           key: 'Gene',
-          value: 'ALDH2'
+          value: 'ALDH2',
         },
         {
           key: 'refSNP',
-          value: 'rs114202595'
+          value: 'rs114202595',
         },
         {
           key: 'TogoVar',
-          value: 'tgv421843'
+          value: 'tgv421843',
         },
         {
           key: 'Position(GRCh37/hg19)',
-          value: '16:48258198'
+          value: '16:48258198',
         },
         {
           key: 'Region(GRCh37/hg19)',
-          value: '10:73270743-73376976'
+          value: '10:73270743-73376976',
         },
         {
           key: 'HGVSc',
-          value: 'NM_000690:c.1510G>A'
+          value: 'NM_000690:c.1510G>A',
         },
         {
           key: 'HGVSp',
-          value: 'ALDH2:p.Glu504Lys'
-        }
+          value: 'ALDH2:p.Glu504Lys',
+        },
       ];
     case 'GRCh38':
       return [
         {
           key: 'Disease',
-          value: 'Breast-ovarian cancer, familial 2'
+          value: 'Breast-ovarian cancer, familial 2',
         },
         {
           key: 'Gene',
-          value: 'ALDH2'
+          value: 'ALDH2',
         },
         {
           key: 'refSNP',
-          value: 'rs114202595'
+          value: 'rs114202595',
         },
         {
           key: 'TogoVar',
-          value: 'tgv421843'
+          value: 'tgv421843',
         },
         {
           key: 'Position(GRCh38)',
-          value: '16:48224287'
+          value: '16:48224287',
         },
         {
           key: 'Region(GRCh38)',
-          value: '10:71510986-71617219'
+          value: '10:71510986-71617219',
         },
         {
           key: 'HGVSc',
-          value: 'NM_000690:c.1510G>A'
+          value: 'NM_000690:c.1510G>A',
         },
         {
           key: 'HGVSp',
-          value: 'ALDH2:p.Glu504Lys'
-        }
+          value: 'ALDH2:p.Glu504Lys',
+        },
       ];
     default:
       return [];
@@ -85,7 +86,8 @@ export default class SimpleSearchView {
       this,
       elm,
       'Search for disease or gene symbol or rs...',
-      ['gene', 'disease']
+      ['gene', 'disease'],
+      `${API_URL}/suggest?term=`
     );
 
     // events
@@ -110,8 +112,9 @@ export default class SimpleSearchView {
     });
   }
 
-  search(value) {
-    StoreManager.setSimpleSearchCondition('term', value);
+  search() {
+    const label = this._searchFieldView.label;
+    StoreManager.setSimpleSearchCondition('term', label);
   }
 
   simpleSearchConditions(conditions) {
