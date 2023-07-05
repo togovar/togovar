b075db9a5ee85ebd5adafa5350e5dc484944f53e
Anton <nokker@gmail.com>
2023-07-05 14:14:26 +0900

    replace gene search with common search field

--- app/frontend/javascripts/components/Common/SearchField/SearchField.js
+++ app/frontend/javascripts/components/Common/SearchField/SearchField.js
@@ -2,20 +2,39 @@ import { LitElement, html, nothing } from 'lit';
 import { map } from 'lit/directives/map.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { Task } from '@lit-labs/task';
-import { ref, createRef } from 'lit/directives/ref.js';
+import { ref, createRef, Ref } from 'lit/directives/ref.js';
 import { debounce } from '../../../utils/debounce';
 import { axios } from '../../../utils/cachedAxios';
-// import { API_URL } from '../../global';
 import { scrollMeUp } from './scrollMeUp';
 
-// const DISEASE_ADVANCED_SUGGEST_URL = `${API_URL}/api/search/disease?term=`;
+/**
+ * @typedef SuggestAPIParams
+ * @type {object}
+ * @property {string} suggestAPIURL
+ * @property {string} suggestAPIQueryParam
+ */
 
-// suggestAPIURL =  DISEASE_ADVANCED_SUGGEST_URL,
-// suggestAPIQueryParam = term
+/**
+ * @typedef SearchFieldOptions
+ * @type {object}
+ * @property {string} id
+ * @property {string} value
+ */
+
+/**
+ * @typedef SuggestSelectEvent
+ * @property {object} detail
+ * @property {string} detail.id
+ * @property {string} detail.value
+ */
 
 export default class SearchField extends LitElement {
-  #getSuggestURL = () => {};
-  inputRef = createRef();
+  #getSuggestURL = (text) => text;
+  /** @type {SearchFieldOptions} */
+  #options = {};
+
+  /** @type {Ref<HTMLInputElement>} */
+  #inputRef = createRef();
 
   apiTask = new Task(
     this,
@@ -29,45 +48,81 @@ export default class SearchField extends LitElement {
       }
       return Promise.resolve(() => (this.showSuggestions = false));
     },
-    () => this.value
+    () => this.label
   );
 
   static properties = {
     value: { type: String, state: true },
+    label: { type: String, state: true },
     showSuggestions: { type: Boolean, state: true },
     currentSuggestionIndex: { type: Number, state: true },
     suggestData: {
       type: Array,
       state: true,
     },
+    placeholder: { type: String },
     suggestAPIURL: { type: String },
     suggestAPIQueryParam: { type: String },
   };
 
-  constructor(placeholder = 'Enter disease name') {
+  /**
+   *
+   * @param {SuggestAPIParams | null} suggestAPIParams
+   * @param {string} placeholder
+   * @param {SearchFieldOptions} options
+   * @param {HTMLElement} element
+   */
+  constructor(
+    suggestAPIParams = null,
+    placeholder = 'Start typing ...',
+    options = { id: 'id', value: 'label' },
+    element = null
+  ) {
     super();
     this.value = '';
-    this.placeholder = placeholder;
+    this.label = '';
+    this.placeholder = '';
     this.showSuggestions = false;
     this.currentSuggestionIndex = -1;
     this.suggestData = null;
     this.highlightIndex = -1;
     this.suggestAPIURL = '';
     this.suggestAPIQueryParam = '';
+    if (element) {
+      // that means is was appended with new keyword
+      this.placeholder = placeholder;
+      this.#options = options;
+      element.appendChild(this);
+      if (suggestAPIParams.suggestAPIURL) {
+        if (suggestAPIParams.suggestAPIQueryParam) {
+          this.#getSuggestURL = (text) => {
+            const url = new URL(suggestAPIParams.suggestAPIURL);
+            url.searchParams.set(suggestAPIParams.suggestAPIQueryParam, text);
+            return url.toString();
+          };
+        } else {
+          this.#getSuggestURL = (text) => {
+            return `${suggestAPIParams.suggestAPIURL}/${text}`;
+          };
+        }
+      }
+    }
   }
 
   _keyup(e) {
     if (e.target && e.target.nodeName === 'INPUT') {
-      this.value = e.target.value;
+      this.label = e.target.value;
     }
   }
 
   _select(suggestion) {
+    this.value = suggestion[this.#options.id];
+    this.label = suggestion[this.#options.value];
     this.dispatchEvent(
       new CustomEvent('new-suggestion-selected', {
         detail: {
-          id: suggestion.id,
-          label: suggestion.label,
+          id: suggestion[this.#options.id],
+          label: suggestion[this.#options.value],
         },
         bubbles: true,
         composed: true,
@@ -106,26 +161,14 @@ export default class SearchField extends LitElement {
   }
 
   willUpdate(changed) {
-    if (changed.has('value')) {
-      if (this.value.length >= 3) {
+    if (changed.has('label')) {
+      if (this.label.length >= 3) {
         this.showSuggestions = true;
       } else {
         this.showSuggestions = false;
       }
     }
-    if (changed.has('suggestAPIURL') && this.suggestAPIURL) {
-      if (this.suggestAPIQueryParam) {
-        this.#getSuggestURL = (text) => {
-          const url = new URL(this.suggestAPIURL);
-          url.searchParams.set(this.suggestAPIQueryParam, text);
-          return url.toString();
-        };
-      } else {
-        this.#getSuggestURL = (text) => {
-          return `${this.suggestAPIURL}${text}`;
-        };
-      }
-    }
+
     if (changed.has('suggestData') && this.suggestData?.length) {
       this.currentSuggestionIndex = -1;
     }
@@ -139,6 +182,33 @@ export default class SearchField extends LitElement {
         }
       }
     }
+    if (changed.has('suggestAPIURL') || changed.has('suggestAPIQueryParam')) {
+      if (this.suggestAPIURL) {
+        if (this.suggestAPIQueryParam) {
+          this.#getSuggestURL = (text) => {
+            const url = new URL(this.suggestAPIURL);
+            url.searchParams.set(this.suggestAPIQueryParam, text);
+            return url.toString();
+          };
+        } else {
+          this.#getSuggestURL = (text) => {
+            return `${this.suggestAPIURL}/${text}`;
+          };
+        }
+      }
+    }
+  }
+
+  get value() {
+    return this.value;
+  }
+
+  get label() {
+    return this.label;
+  }
+
+  setTerm(term) {
+    this.label = term;
   }
 
   createRenderRoot() {
@@ -151,10 +221,10 @@ export default class SearchField extends LitElement {
         <div class="fieldcontainer">
           <div class="field">
             <input
-              ${ref(this.inputRef)}
+              ${ref(this.#inputRef)}
               type="text"
               placeholder="${this.placeholder}"
-              value="${this.value}"
+              value="${this.label}"
               @input="${debounce(this._keyup, 300)}"
               @focusout="${this._hideSuggestions}"
               @focusin="${() => {
