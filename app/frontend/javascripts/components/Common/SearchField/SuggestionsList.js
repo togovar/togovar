992ff92ec3769ab562799c6f709342dcabd5ef9e
Anton <nokker@gmail.com>
2023-07-06 20:22:54 +0900

    add lit scss support, add common component to DAG

---
+++ app/frontend/javascripts/components/Common/SearchField/SuggestionsList.js
@@ -0,0 +1,73 @@
+import { LitElement, html } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import Styles from '../../../../stylesheets/object/component/suggestions-list.scss';
+import { scrollMeUp } from './scrollMeUp';
+
+export default class SuggestionsList extends LitElement {
+  static get properties() {
+    return {
+      /** Suggestions data */
+      suggestData: { type: Array },
+      /** Highlighted item's index (by keys) */
+      highlightedSuggestionIndex: {
+        type: Number,
+        attribute: 'highlighted-suggestion-index',
+      },
+      /** What of an item to map to dispatched event's detail.id */
+      itemIdKey: { type: String, attribute: 'item-id-key' },
+      /** What of an item to map to dispatched event's detail.label */
+      itemLabelKey: { type: String, attribute: 'item-label-key' },
+    };
+  }
+
+  static get styles() {
+    return [Styles];
+  }
+
+  constructor() {
+    super();
+    this.suggestData = [];
+    this.highlightedSuggestionIndex = -1;
+    this.itemIdKey = '';
+    this.itemLabelKey = '';
+  }
+
+  #handleSelect(item) {
+    this.dispatchEvent(
+      new CustomEvent('suggestion-selected', {
+        detail: item,
+        bubbles: true,
+        composed: true,
+      })
+    );
+  }
+
+  render() {
+    return html`
+      <ul class="list">
+        ${repeat(
+          this.suggestData,
+          (item) => item[this.itemIdKey],
+          (item, index) => html`
+            <li
+              class="item ${this.highlightedSuggestionIndex === index
+                ? '-selected'
+                : ''}"
+              @mousedown="${() => this.#handleSelect(item)}"
+              ${scrollMeUp(this.highlightedSuggestionIndex === index)}
+            >
+              ${unsafeHTML(item?.highlight || item?.[this.itemLabelKey])}
+            </li>
+          `
+        )}
+      </ul>
+    `;
+  }
+
+  //   createRenderRoot() {
+  //     return this;
+  //   }
+}
+
+customElements.define('suggestions-list', SuggestionsList);
