cadcd67f127c92b44188fd114d76fa2daaf8cc80
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2023-07-18 20:12:58 +0900

    quick fix when root disease node is selected

--- app/lib/togo_var/api/models/version1/disease.rb
+++ app/lib/togo_var/api/models/version1/disease.rb
@@ -16,15 +16,23 @@ module TogoVar
           def to_hash
             validate
 
-            terms = @sub_concepts ? @terms.map { |x| [x, DiseaseMondo.sub_concepts(x)] }.flatten.uniq : @terms
-            terms = terms.map { |x| x.start_with?('MONDO_') ? DiseaseMondo.mondo2cui(x) : x }.flatten.uniq
+            terms = @terms
+            sub_concepts = @sub_concepts
 
+            # TODO: improve elasticsearch indexing for searching concept hierarchy
             q = Elasticsearch::DSL::Search.search do
               query do
                 nested do
                   path 'clinvar.conditions'
                   query do
-                    terms 'clinvar.conditions.medgen': terms
+                    if terms.any? { |x| %w[MONDO_0000001 C0012634].include?(x&.strip) }
+                      exists field: 'clinvar.conditions.medgen'
+                    else
+                      terms = sub_concepts ? terms.map { |x| [x, DiseaseMondo.sub_concepts(x)] }.flatten.uniq : terms
+                      terms = terms.map { |x| x.start_with?('MONDO_') ? DiseaseMondo.mondo2cui(x) : x }.flatten.uniq
+
+                      terms 'clinvar.conditions.medgen': terms
+                    end
                   end
                 end
               end
