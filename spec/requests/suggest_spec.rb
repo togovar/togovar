67c101586d907a9f41a7af2f6cf4b2d3e7bd214f
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2023-10-30 13:36:10 +0900

    update

--- spec/requests/suggest_spec.rb
+++ spec/requests/suggest_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Suggests", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Suggests', type: :request do
+  describe 'GET /index' do
     it 'suggests BRCA2' do
-      get "/suggest", params: { term: 'BRCA2' }, headers: { Accept: "application/json" }
+      get '/suggest', params: { term: 'BRCA2' }, headers: { Accept: 'application/json' }
 
       expect(response).to have_http_status(:ok)
       expect(response.content_type).to eq('application/json; charset=utf-8')
@@ -13,15 +13,15 @@ RSpec.describe "Suggests", type: :request do
       expect(json['gene']).to include('term' => 'BRCA2', 'alias_of' => nil)
     end
 
-    it 'suggests Breast-ovarian cancer, familial 1' do
-      get "/suggest", params: { term: 'breast-ovarian' }, headers: { Accept: "application/json" }
+    it 'suggests Breast-ovarian cancer, familial, susceptibility to, 1' do
+      get '/suggest', params: { term: 'breast-ovarian' }, headers: { Accept: 'application/json' }
 
       expect(response).to have_http_status(:ok)
       expect(response.content_type).to eq('application/json; charset=utf-8')
 
       json = JSON.parse(response.body)
       expect(json.keys).to match_array(%w[gene disease])
-      expect(json['disease']).to include('term' => 'Breast-ovarian cancer, familial 1')
+      expect(json['disease']).to include('term' => 'Breast-ovarian cancer, familial, susceptibility to, 1')
     end
   end
 end
