df2884589230a31964d5a72c669a0af013aa2ffb
永野 朗夫 <nagano@me.com>
2023-11-01 17:59:38 +0900

    fix over requests

--- app/frontend/javascripts/classes/ScrollBar.js
+++ app/frontend/javascripts/classes/ScrollBar.js
@@ -1,21 +1,23 @@
-import StoreManager from "./StoreManager.js";
-import {TR_HEIGHT} from '../global.js';
+import StoreManager from './StoreManager.js';
+import { TR_HEIGHT } from '../global.js';
 
 const RELEASE_DURATION = 2000;
 const MIN_HEIGHT = 30;
 
 export default class ScrollBar {
-
   constructor(elm) {
     this.elm = elm;
-    this.elm.insertAdjacentHTML('beforeend', `
+    this.elm.insertAdjacentHTML(
+      'beforeend',
+      `
       <div class="bar">
         <div class="indicator">
           <span class="position">1</span>
           <span class="total"></span>
         </div>
       </div>
-    `);
+    `
+    );
     this.bar = this.elm.querySelector('.bar');
     this.position = this.bar.querySelector('.position');
     this.total = this.bar.querySelector('.total');
@@ -29,21 +31,19 @@ export default class ScrollBar {
       axis: 'y',
       containment: this.elm,
       cursor: 'grab',
-      drag: this.drag.bind(this)
+      drag: this.drag.bind(this),
     });
-
   }
 
   drag(e, ui) {
-    const
-      rowCount = StoreManager.getData('rowCount'),
+    const rowCount = StoreManager.getData('rowCount'),
       numberOfRecords = StoreManager.getData('numberOfRecords'),
       availableHeight = rowCount * TR_HEIGHT - this.bar.offsetHeight * 0,
       offsetRate = ui.position.top / availableHeight;
-    let
-      offset = Math.ceil(offsetRate * numberOfRecords);
+    let offset = Math.ceil(offsetRate * numberOfRecords);
     offset = offset < 0 ? 0 : offset;
-    offset = (offset + rowCount) > numberOfRecords ? numberOfRecords - rowCount : offset;
+    offset =
+      offset + rowCount > numberOfRecords ? numberOfRecords - rowCount : offset;
     StoreManager.setData('offset', offset);
     this.prepareRelease();
   }
@@ -63,19 +63,16 @@ export default class ScrollBar {
   }
 
   update() {
-    const
-      offset = StoreManager.getData('offset'),
+    const offset = StoreManager.getData('offset'),
       rowCount = StoreManager.getData('rowCount'),
       numberOfRecords = StoreManager.getData('numberOfRecords'),
       totalHeight = numberOfRecords * TR_HEIGHT, // 全体の高さ
       offsetHeight = offset * TR_HEIGHT, // オフセット量
       displayHeight = rowCount * TR_HEIGHT, // 表示領域
       displayRate = displayHeight / totalHeight;
-    let
-      barHeight = Math.ceil(displayHeight * displayRate);
+    let barHeight = Math.ceil(displayHeight * displayRate);
     barHeight = barHeight < MIN_HEIGHT ? MIN_HEIGHT : barHeight;
-    const
-      availableHeight = displayHeight - barHeight * 0,
+    const availableHeight = displayHeight - barHeight * 0,
       availableRate = availableHeight / totalHeight,
       barTop = Math.ceil(offsetHeight * availableRate);
     this.bar.style.height = `${barHeight}px`;
@@ -90,12 +87,14 @@ export default class ScrollBar {
 
   prepareRelease() {
     window.clearTimeout(this.timeoutId);
-    this.timeoutId = window.setTimeout(this.release.bind(this), RELEASE_DURATION);
+    this.timeoutId = window.setTimeout(
+      this.release.bind(this),
+      RELEASE_DURATION
+    );
     this.elm.classList.add('-dragging');
   }
 
   release() {
     this.elm.classList.remove('-dragging');
   }
-
 }
