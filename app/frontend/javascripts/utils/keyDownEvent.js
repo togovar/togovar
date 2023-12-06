c3a850d5f1d2a603a07aaaf3e0959505819af2c6
Yukiko Noda <yukiko8318@gmail.com>
2023-12-06 16:40:26 +0900

    fix  keyDownEvent

--- app/frontend/javascripts/utils/keyDownEvent.js
+++ app/frontend/javascripts/utils/keyDownEvent.js
@@ -1,18 +1,15 @@
 import StoreManager from '../classes/StoreManager.js';
 
 export function keyDownEvent(storeName) {
-  let showModal = StoreManager.getData('showModal');
-  let showSuggest = StoreManager.getData('showSuggest');
-
   switch (storeName) {
     case 'showModal':
-      if (showSuggest !== true) {
+      if (StoreManager.getData('showSuggest')) {
         return true;
       }
       return false;
 
     case 'selectedRow':
-      if (showModal !== true) {
+      if (StoreManager.getData('showModal')) {
         return true;
       }
       return false;
