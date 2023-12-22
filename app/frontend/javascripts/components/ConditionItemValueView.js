7d5dc2b5d0c2ca118e5b130f051d23a7ebe8df9d
Yukiko Noda <yukiko8318@gmail.com>
2023-12-22 15:48:55 +0900

    fix condition-item-value-view

--- app/frontend/javascripts/components/ConditionItemValueView.js
+++ app/frontend/javascripts/components/ConditionItemValueView.js
@@ -1,91 +1,10 @@
-import { LitElement, html, css, nothing } from 'lit';
+import { LitElement, html, nothing } from 'lit';
 import './FrequencyCountValueView'; // for embedding
 import Style from '../../stylesheets/object/component/condition-item-value-view.scss';
 
 export default class ConditionItemValueView extends LitElement {
-  static styles = [
-    css`
-      :host {
-        --height-container: var(
-          --height-advanced-search-condition-values-container
-        );
-        --height: var(--height-advanced-search-condition-value);
-        display: inline-block;
-      }
-      :host([data-condition-type='dataset']) {
-        display: block;
-        position: relative;
-      }
-      :host > .inner {
-        height: var(--height);
-        line-height: 1;
-        padding: 0 10px;
-        background-color: white;
-        border: solid 1px var(--color-key-dark1);
-        border-radius: calc(var(--height) * 0.5);
-        font-weight: bold;
-        margin-right: 2px;
-      }
-      :host([data-condition-type='dataset']) > .inner {
-        min-width: 120px;
-      }
-      :host([data-condition-type='dataset']) > .inner::before {
-        font-family: fontello;
-        content: var(--char-dataset);
-        margin-right: 4px;
-      }
-      :host([data-value='jga_ngs']) > .inner,
-      :host([data-value='jga_snp']) > .inner {
-        border-color: var(--color-dataset-jga);
-        background-color: var(--color-dataset-jga-light);
-      }
-      :host([data-value='jga_ngs']) > .inner::before,
-      :host([data-value='jga_snp']) > .inner::before {
-        color: var(--color-dataset-jga);
-      }
-      :host([data-value='tommo']) > .inner {
-        border-color: var(--color-dataset-tommo);
-        background-color: var(--color-dataset-tommo-light);
-      }
-      :host([data-value='tommo']) > .inner::before {
-        color: var(--color-dataset-tommo);
-      }
-      :host([data-value='hgvd']) > .inner,
-      :host([data-value='mgend']) > .inner {
-        border-color: var(--color-dataset-hgvd);
-        background-color: var(--color-dataset-hgvd-light);
-      }
-      :host([data-value='hgvd']) > .inner::before,
-      :host([data-value='mgend']) > .inner::before {
-        color: var(--color-dataset-hgvd);
-      }
-      :host([data-value='gem_j_wga']) > .inner {
-        border-color: var(--color-dataset-gemj);
-        background-color: var(--color-dataset-gemj-light);
-      }
-      :host([data-value='gem_j_wga']) > .inner::before {
-        color: var(--color-dataset-gemj);
-      }
-      :host([data-value='bbj']) > .inner {
-        border-color: var(--color-dataset-bbj);
-        background-color: var(--color-dataset-bbj-light);
-      }
-      :host([data-value='bbj']) > .inner::before {
-        color: var(--color-dataset-bbj);
-      }
-      :host([data-value='clinvar']) > .inner,
-      :host([data-value='exac']) > .inner,
-      :host([data-value='gnomad']) > .inner {
-        border-color: var(--color-dataset-foreign);
-        background-color: var(--color-dataset-foreign-light);
-      }
-      :host([data-value='clinvar']) > .inner::before,
-      :host([data-value='exac']) > .inner::before,
-      :host([data-value='gnomad']) > .inner::before {
-        color: var(--color-dataset-foreign);
-      }
-    `, Style,
-  ];
+  // static styles = [css`${ Style }`];
+  static styles = [Style];
 
   static properties = {
     label: { type: String },
