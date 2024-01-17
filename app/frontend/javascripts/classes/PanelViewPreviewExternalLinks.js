4e7cef1ccb4820e3abc1f52abe5b743e0b1a024a
Yukiko Noda <yukiko8318@gmail.com>
2024-01-17 15:03:33 +0900

    add gnomAD link

--- app/frontend/javascripts/classes/PanelViewPreviewExternalLinks.js
+++ app/frontend/javascripts/classes/PanelViewPreviewExternalLinks.js
@@ -27,18 +27,40 @@ export default class PanelViewPreviewExternalLinks extends PanelView {
       if (record && record.external_link) {
         // resSNP
         if (record.external_link.dbsnp) {
-          list.push({
-            title: 'refSNP',
-            content: `<ul>${(() => record.external_link.dbsnp.map(rs => `<li><a href="http://identifiers.org/dbsnp/${rs}" class="hyper-text -external" target="_blank">${rs}</a></li>`).join(''))()}</ul>`
-          });
+          list.push(
+            this.#createLinkList(
+              'refSNP',
+              record.external_link.dbsnp,
+              rs => `http://identifiers.org/dbsnp/${rs}`
+            )
+          );
         }
+
         // ClinVar
         if (record.external_link.clinvar) {
-          list.push({
-            title: 'ClinVar',
-            content: `<ul>${(() => record.external_link.clinvar.map(vcv => `<li><a href="https://www.ncbi.nlm.nih.gov/clinvar/variation/${parseInt(vcv.replace('VCV', ''))}" class="hyper-text -external" target="_blank">${vcv}</a></li>`).join(''))()}</ul>`
-          });
+          list.push(
+            this.#createLinkList(
+              'ClinVar',
+              record.external_link.clinvar,
+              vcv => `https://www.ncbi.nlm.nih.gov/clinvar/variation/${parseInt(vcv.replace('VCV', ''))}`
+            )
+          );
         }
+
+        // gnomAD
+        const recordFrequencies = record.frequencies.map(
+          frequency => frequency.source
+        );
+        if (recordFrequencies.includes('gnomad_genomes')) {
+          list.push(
+            this.#createLinkList(
+              'gnomAD',
+              [`${record.chromosome}-${record.position}-${record.reference}-${record.alternate}`],
+              variant => `https://gnomad.broadinstitute.org/variant/${variant}`
+            )
+          );
+        }
+
         if (list.length > 0) {
           html = `
             <tbody>
@@ -52,4 +74,8 @@ export default class PanelViewPreviewExternalLinks extends PanelView {
     this.table.innerHTML = html;
   }
 
+  #createLinkList(title, data, getUrl) {
+    const content = `<ul>${data.map(item =>`<li><a href="${getUrl(item)}" class="hyper-text -external" target="_blank">${item}</a></li>`).join('')}</ul>`;
+    return { title, content };
+  }
 }
