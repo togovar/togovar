e95fc4c1ebbff3a96756f9efb8818fb5ff6b4cd9
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2024-02-26 17:43:40 +0900

    fix bug

--- app/models/form/dataset.rb
+++ app/models/form/dataset.rb
@@ -1,23 +1,7 @@
 module Form
   class Dataset < ParameterBase
-    case ENV.fetch('TOGOVAR_REFERENCE')
-    when 'GRCh37'
-      register(:gem_j_wga, 'GEM-J WGA', 'gem_j_wga', '1')
-      register(:jga_ngs, 'JGA-NGS', 'jga_ngs', '1')
-      register(:jga_snp, 'JGA-SNP', 'jga_snp', '1')
-      register(:tommo, 'ToMMo 8.3KJPN', 'tommo', '1')
-      register(:hgvd, 'HGVD', 'hgvd', '1')
-      register(:gnomad_genomes, 'gnomAD Genomes', 'gnomad_genomes', '1')
-      register(:gnomad_exomes, 'gnomAD Exomes', 'gnomad_exomes', '1')
-      register(:clinvar, 'ClinVar', 'clinvar', '1')
-    when 'GRCh38'
-      register(:gem_j_wga, 'GEM-J WGA', 'gem_j_wga', '1')
-      register(:jga_ngs, 'JGA-NGS', 'jga_ngs', '1')
-      register(:jga_snp, 'JGA-SNP', 'jga_snp', '1')
-      register(:tommo, 'ToMMo 14KJPN', 'tommo', '1')
-      register(:hgvd, 'HGVD', 'hgvd', '1')
-      register(:gnomad_genomes, 'gnomAD Genomes', 'gnomad_genomes', '1')
-      register(:clinvar, 'ClinVar', 'clinvar', '1')
+    Variation::Datasets::ALL.each do |x|
+      register(x.to_sym, x, x, '1')
     end
   end
 end
