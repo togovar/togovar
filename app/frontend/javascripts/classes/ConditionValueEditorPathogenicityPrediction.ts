882315687393e63bea2bd860d802d86313b0964f
Yukiko Noda <yukiko8318@gmail.com>
2024-11-19 16:44:33 +0900

    fix private class

--- app/frontend/javascripts/classes/ConditionValueEditorPathogenicityPrediction.ts
+++ app/frontend/javascripts/classes/ConditionValueEditorPathogenicityPrediction.ts
@@ -23,13 +23,13 @@ interface PredictionValueViewElement extends HTMLElement {
 
 /** Pathogenicity prediction editing screen */
 class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
-  private _dataset: string; // selected dataset (e.g., alphamissense, sift, polyphen)
-  private _label: string; // selected label (e.g., AlphaMissense, SIFT, PolyPhen)
-  private _values: Array<number>; // max-min values (0〜1)
-  private _inequalitySigns: Array<string>; // max-min inequality signs (gte, gt, lte, lt)
-  private _unassignedChecks: Array<string>; // unassigned checks (e.g., unassigned, unknown)
-  private _tabsContainer: HTMLDivElement; // container for tabs
-  private _lastState: {
+  #dataset: string; // selected dataset (e.g., alphamissense, sift, polyphen)
+  #label: string; // selected label (e.g., AlphaMissense, SIFT, PolyPhen)
+  #values: Array<number>; // max-min values (0〜1)
+  #inequalitySigns: Array<string>; // max-min inequality signs (gte, gt, lte, lt)
+  #unassignedChecks: Array<string>; // unassigned checks (e.g., unassigned, unknown)
+  #tabsContainer: HTMLDivElement; // container for tabs
+  #lastState: {
     dataset: string;
     label: string;
     values: Array<number>;
@@ -43,50 +43,50 @@ class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
    */
   constructor(valuesView: ConditionValues, conditionView: ConditionItemView) {
     super(valuesView, conditionView);
-    this._initializeDefaultValues();
-    this._initializeUI();
-    this._initializeEvents();
+    this.#initializeDefaultValues();
+    this.#initializeUI();
+    this.#initializeEvents();
   }
 
   // Initialize default values
-  private _initializeDefaultValues() {
-    this._dataset = 'alphamissense';
-    this._label = 'AlphaMissense';
-    this._values = [0, 1];
-    this._inequalitySigns = ['gte', 'lte'];
-    this._unassignedChecks = [];
-    this._lastState = {
-      dataset: this._dataset,
-      label: this._label,
-      values: [...this._values],
-      inequalitySigns: [...this._inequalitySigns],
-      unassignedChecks: [...this._unassignedChecks],
+  #initializeDefaultValues() {
+    this.#dataset = 'alphamissense';
+    this.#label = 'AlphaMissense';
+    this.#values = [0, 1];
+    this.#inequalitySigns = ['gte', 'lte'];
+    this.#unassignedChecks = [];
+    this.#lastState = {
+      dataset: this.#dataset,
+      label: this.#label,
+      values: [...this.#values],
+      inequalitySigns: [...this.#inequalitySigns],
+      unassignedChecks: [...this.#unassignedChecks],
     };
   }
 
   // Initialize UI elements
-  private _initializeUI() {
+  #initializeUI() {
     this._createElement(
       'pathogenicity-editor-view',
       `<header>Select prediction</header><div class="body" />`
     );
-    this._tabsContainer = this._el.querySelector('.body')!;
-    this._createTabView();
+    this.#tabsContainer = this._el.querySelector('.body')!;
+    this.#createTabView();
   }
 
   // Initialize event listeners
-  private _initializeEvents() {
-    this._tabsContainer.addEventListener(
+  #initializeEvents() {
+    this.#tabsContainer.addEventListener(
       'set-prediction-values',
       (e: Event) => {
         const customEvent = e as CustomEvent;
-        this._updateValuesAndSigns(customEvent.detail);
+        this.#updateValuesAndSigns(customEvent.detail);
       }
     );
 
-    this._tabsContainer.addEventListener('switch-tab', (e: Event) => {
+    this.#tabsContainer.addEventListener('switch-tab', (e: Event) => {
       const customEvent = e as CustomEvent;
-      this._switchTab(customEvent.detail);
+      this.#switchTab(customEvent.detail);
     });
   }
 
@@ -102,7 +102,7 @@ class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
         if (conditionValues) {
           const { dataset, label, values, inequalitySigns, unassignedChecks } =
             conditionValues;
-          this._lastState = {
+          this.#lastState = {
             dataset,
             label,
             values,
@@ -116,8 +116,8 @@ class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
   /** Restore the last selected values if editing is canceled */
   restore() {
     const { dataset, label, values, inequalitySigns, unassignedChecks } =
-      this._lastState;
-    this._addPredictionValueView(
+      this.#lastState;
+    this.#addPredictionValueView(
       dataset,
       label,
       values,
@@ -128,26 +128,26 @@ class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
 
   // Private methods
   /** Update UI and values when a tab is switched */
-  private _switchTab(detail: any) {
-    this._dataset = detail.dataset;
-    this._label = PREDICTIONS[this._dataset].label;
-    this._updateValuesAndSigns(detail);
+  #switchTab(detail: any) {
+    this.#dataset = detail.dataset;
+    this.#label = PREDICTIONS[this.#dataset].label;
+    this.#updateValuesAndSigns(detail);
   }
 
   /** Update internal values and render */
-  private _update() {
-    this._addPredictionValueView(
-      this._dataset,
-      this._label,
-      this._values,
-      this._inequalitySigns,
-      this._unassignedChecks
+  #update() {
+    this.#addPredictionValueView(
+      this.#dataset,
+      this.#label,
+      this.#values,
+      this.#inequalitySigns,
+      this.#unassignedChecks
     );
-    this._valuesView.update(this._validate());
+    this._valuesView.update(this.#validate());
   }
 
   /** Add or update the value view */
-  private _addPredictionValueView(
+  #addPredictionValueView(
     dataset: string,
     label: string,
     values: Array<number>,
@@ -185,32 +185,32 @@ class ConditionValueEditorPathogenicityPrediction extends ConditionValueEditor {
   }
 
   /** Validate if the values are valid */
-  private _validate(): boolean {
-    return this._values.filter((item) => !Number.isNaN(item)).length === 2;
+  #validate(): boolean {
+    return this.#values.filter((item) => !Number.isNaN(item)).length === 2;
   }
 
   /** Create tab view */
-  private _createTabView() {
+  #createTabView() {
     const tabView = document.createElement('tab-view') as any;
     tabView.datasets = PREDICTIONS;
-    this._tabsContainer.appendChild(tabView);
+    this.#tabsContainer.appendChild(tabView);
   }
 
   /** Update values and inequality signs */
-  private _updateValuesAndSigns(detail: any) {
-    this._values = [detail.values[0], detail.values[1]];
-    this._inequalitySigns = [
+  #updateValuesAndSigns(detail: any) {
+    this.#values = [detail.values[0], detail.values[1]];
+    this.#inequalitySigns = [
       detail.inequalitySigns[0],
       detail.inequalitySigns[1],
     ];
-    this._unassignedChecks = detail.unassignedChecks;
-    this._update();
+    this.#unassignedChecks = detail.unassignedChecks;
+    this.#update();
   }
 
   // Accessor
   /** You can press the OK button if there are two valid values */
   get isValid() {
-    return this._values.filter((item) => !Number.isNaN(item)).length === 2;
+    return this.#values.filter((item) => !Number.isNaN(item)).length === 2;
   }
 }
 
