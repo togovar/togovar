0379b933eac14139e8384cf3d83f74916739e37b
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2024-12-20 20:35:12 +0900

    remove backend

--- app/lib/togo_var/api.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'togo_var/api/models'
-require 'togo_var/api/spec'
-require 'togo_var/api/variation_search'
--- app/lib/togo_var/common_options.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module TogoVar
-  module CommonOptions
-    SENTENCE_OR_CONNECTORS = { words_connector: "', '",
-                               two_words_connector: "' or '",
-                               last_word_connector: "', or '" }.freeze
-  end
-end
