64deb49dd587ab3132c3ab7caba3c298dafeae78
行事光 <h.gyoji.vrds@gmail.com>
2025-04-15 15:03:17 +0900

    add term condition

--- app/frontend/src/store/searchManager.ts
+++ app/frontend/src/store/searchManager.ts
@@ -115,12 +115,17 @@ export function reflectSimpleSearchConditionToURI() {
   const currentConditions = storeManager.getData('simpleSearchConditions');
   const diffConditions = extractSearchCondition(currentConditions);
 
-  // 現在のURLパラメータを保持しながら更新
-  _currentUrlParams = {
-    ..._currentUrlParams, // 既存のパラメータを保持
-    mode: 'simple', // modeは必ず'simple'に
-    ...diffConditions, // 新しい検索条件で上書き
-  };
+  // 検索条件が空の場合やtermが空の場合は、URLパラメータをクリア
+  if (currentConditions.term === '') {
+    _currentUrlParams = { mode: 'simple' };
+  } else {
+    // 現在のURLパラメータを保持しながら更新
+    _currentUrlParams = {
+      ..._currentUrlParams, // 既存のパラメータを保持
+      mode: 'simple', // modeは必ず'simple'に
+      ...diffConditions, // 新しい検索条件で上書き
+    };
+  }
 
   //URLを更新 (ブラウザの履歴に新しい状態を追加)
   const newUrl = `${window.location.origin}${
