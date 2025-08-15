d4215568acec12b79814c2ba3b31300ff2ed7845
Yukiko Noda <yukiko8318@gmail.com>
2025-08-15 16:14:54 +0900

    separate SimpleSearchView

---
+++ app/frontend/src/components/Common/SearchField/SimpleSearchConstants.js
@@ -0,0 +1,102 @@
+import { API_URL } from '../../../global.js';
+
+/** SimpleSearchConstants - SimpleSearchView関連の定数定義 */
+
+/** 検索例のデータ */
+export const EXAMPLES = (() => {
+  switch (TOGOVAR_FRONTEND_REFERENCE) {
+    case 'GRCh37':
+      return [
+        {
+          key: 'Disease',
+          value: 'Breast-ovarian cancer, familial 2',
+        },
+        {
+          key: 'Gene',
+          value: 'ALDH2',
+        },
+        {
+          key: 'refSNP',
+          value: 'rs114202595',
+        },
+        {
+          key: 'TogoVar',
+          value: 'tgv56616325',
+        },
+        {
+          key: 'Position(GRCh37/hg19)',
+          value: '16:48258198',
+        },
+        {
+          key: 'Region(GRCh37/hg19)',
+          value: '10:73270743-73376976',
+        },
+        {
+          key: 'HGVSc',
+          value: 'NM_000690:c.1510G>A',
+        },
+        {
+          key: 'HGVSp',
+          value: 'ALDH2:p.Glu504Lys',
+        },
+      ];
+    case 'GRCh38':
+      return [
+        {
+          key: 'Disease',
+          value: 'Breast-ovarian cancer, familial 2',
+        },
+        {
+          key: 'Gene',
+          value: 'ALDH2',
+        },
+        {
+          key: 'refSNP',
+          value: 'rs114202595',
+        },
+        {
+          key: 'TogoVar',
+          value: 'tgv56616325',
+        },
+        {
+          key: 'Position(GRCh38)',
+          value: '16:48224287',
+        },
+        {
+          key: 'Region(GRCh38)',
+          value: '10:71510986-71617219',
+        },
+        {
+          key: 'HGVSc',
+          value: 'NM_000690:c.1510G>A',
+        },
+        {
+          key: 'HGVSp',
+          value: 'ALDH2:p.Glu504Lys',
+        },
+      ];
+    default:
+      return [];
+  }
+})();
+
+/** 検索フィールドの設定 */
+export const SEARCH_FIELD_CONFIG = {
+  placeholder: 'Search for disease or gene symbol or rs...',
+  suggestAPIURL: `${API_URL}/suggest`,
+  suggestAPIQueryParam: 'term',
+  options: {
+    valueMappings: {
+      valueKey: 'term',
+      labelKey: 'term',
+      aliasOfKey: 'alias_of',
+    },
+    titleMappings: {
+      gene: 'Gene names',
+      disease: 'Disease names',
+    },
+  },
+};
+
+/** 染色体パターンの正規表現 */
+export const CHROMOSOME_PATTERN = /([1-9]|1[0-9]|2[0-2]|X|Y|M|MT):\d+/i;
---
+++ app/frontend/src/components/Common/SearchField/SimpleSearchController.js
@@ -0,0 +1,95 @@
+import {
+  setSimpleSearchCondition,
+  getSimpleSearchCondition,
+} from '../../../store/searchManager';
+import { storeManager } from '../../../store/StoreManager';
+import { CHROMOSOME_PATTERN } from './SimpleSearchConstants';
+
+/**
+ * SimpleSearchController - SimpleSearchViewのビジネスロジックを担当
+ */
+export class SimpleSearchController {
+  constructor(host) {
+    this.host = host;
+  }
+
+  /**
+   * 検索処理
+   * 染色体パターンの正規化も行う
+   * @param {string} term - 検索語
+   */
+  search(term) {
+    if (CHROMOSOME_PATTERN.test(term)) {
+      term = term.replace(/Chr|ch|Cr|cs/i, '').toUpperCase();
+
+      if (term.includes('M:')) {
+        term = term.replace('M:', 'MT:');
+      }
+    }
+
+    setSimpleSearchCondition('term', term);
+  }
+
+  /**
+   * 入力項目の更新
+   * @param {string} term - 入力値
+   */
+  updateTerm(term) {
+    this.host._term = term;
+
+    // 入力されたテキストを検索条件に反映する（ただし検索は実行しない）
+    const currentConditions = {
+      ...storeManager.getData('simpleSearchConditions'),
+    };
+    // 空文字列も適切に処理
+    currentConditions.term = term || '';
+    storeManager.setData('simpleSearchConditions', currentConditions);
+  }
+
+  /** 検索状態のリセット */
+  reset() {
+    this.host._term = '';
+    this.host._value = '';
+    this.host._hideSuggestions = true;
+
+    // 検索条件を更新して空の検索を実行
+    setSimpleSearchCondition('term', '');
+  }
+
+  /**
+   * サジェスト選択時の処理
+   * @param {Object} suggestion - 選択されたサジェスト
+   */
+  selectSuggestion(suggestion) {
+    this.host._hideSuggestions = true;
+    this.host._value = suggestion.label;
+    this.host._term = suggestion.label;
+    this.search(suggestion.label);
+  }
+
+  /**
+   * 例文選択時の処理
+   * @param {Object} example - 選択された例文
+   */
+  selectExample(example) {
+    this.host._hideSuggestions = true;
+    this.host._value = example.value;
+    this.host._term = example.value;
+    this.search(example.value);
+  }
+
+  /**
+   * 現在の検索項目を使用して検索実行
+   */
+  executeCurrentSearch() {
+    if (this.host._term === undefined) return;
+    this.search(this.host._term);
+  }
+
+  /**
+   * ボタンクリック時の検索実行
+   */
+  executeButtonSearch() {
+    this.search(this.host._term || '');
+  }
+}
