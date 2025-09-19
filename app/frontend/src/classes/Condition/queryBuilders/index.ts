699367134dcd0c9d2a17761e0750394253e8aeeb
Yukiko Noda <yukiko8318@gmail.com>
2025-09-19 14:50:14 +0900

    fix query order of index.ts

--- app/frontend/src/classes/Condition/queryBuilders/index.ts
+++ app/frontend/src/classes/Condition/queryBuilders/index.ts
@@ -1,21 +1,21 @@
-import type { ConditionQuery, Builder, BuildContext } from '../../../types';
 import { CONDITION_TYPE, type ConditionTypeValue } from '../../../definition';
 import { buildDatasetQuery } from './dataset';
-import { buildPathogenicityQuery } from './pathogenicity';
-import { buildLocationQuery } from './location';
-import { buildGeneQuery } from './gene';
-import { buildVariantIdQuery } from './variantId';
 import { buildSignificanceQuery } from './significance';
+import { buildGeneQuery } from './gene';
+import { buildLocationQuery } from './location';
+import { buildPathogenicityQuery } from './pathogenicity';
+import { buildVariantIdQuery } from './variantId';
 import { buildDefaultQuery } from './default';
+import type { ConditionQuery, Builder, BuildContext } from '../../../types';
 
 const BUILDERS: Partial<Record<ConditionTypeValue, Builder>> = {
   [CONDITION_TYPE.dataset]: buildDatasetQuery,
-  [CONDITION_TYPE.genotype]: buildDatasetQuery,
-  [CONDITION_TYPE.pathogenicity_prediction]: buildPathogenicityQuery,
-  [CONDITION_TYPE.location]: buildLocationQuery,
-  [CONDITION_TYPE.gene_symbol]: buildGeneQuery, // NOTE: value = 'gene'
-  [CONDITION_TYPE.variant_id]: buildVariantIdQuery, // NOTE: value = 'id'
   [CONDITION_TYPE.significance]: buildSignificanceQuery,
+  [CONDITION_TYPE.gene_symbol]: buildGeneQuery,
+  [CONDITION_TYPE.genotype]: buildDatasetQuery,
+  [CONDITION_TYPE.location]: buildLocationQuery,
+  [CONDITION_TYPE.pathogenicity_prediction]: buildPathogenicityQuery,
+  [CONDITION_TYPE.variant_id]: buildVariantIdQuery,
 };
 
 /** Dispatch to the appropriate builder based on condition type */
