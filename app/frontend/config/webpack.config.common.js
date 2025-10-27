f71b816b376a91a903665f67a605a52b08fe6b90
Yukiko Noda <yukiko8318@gmail.com>
2025-10-27 11:21:33 +0900

    remove gtm

--- app/frontend/config/webpack.config.common.js
+++ app/frontend/config/webpack.config.common.js
@@ -29,7 +29,7 @@ const config = {
         use: {
           loader: 'pug-loader',
           options: {
-            globals: ['GLOBALS', 'TOGOVAR_FRONTEND_GTM_ID'],
+            globals: ['GLOBALS'],
             pretty: true,
           },
         },
@@ -129,11 +129,6 @@ const config = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      TOGOVAR_FRONTEND_GTM_ID: JSON.stringify(
-        process.env.TOGOVAR_FRONTEND_GTM_ID || ''
-      ),
-    }),
     new HtmlWebpackPlugin({
       template: 'app/frontend/views/index.pug',
       filename: 'index.html',
