f97be361726c5bb962d4c5426e8fef54d672fa22
Yukiko Noda <yukiko8318@gmail.com>
2026-05-22 16:07:27 +0900

    fix buildCspDirectives

--- app/frontend/server/middlewares/securityHeaders.js
+++ app/frontend/server/middlewares/securityHeaders.js
@@ -1,20 +1,27 @@
+const crypto = require('crypto');
+
 const ONE_YEAR_SECONDS = 31536000;
 
-const CSP_DIRECTIVES = [
-  "default-src 'self'",
-  "base-uri 'self'",
-  "object-src 'none'",
-  "frame-ancestors 'none'",
-  "form-action 'self'",
-  "script-src 'self' 'unsafe-inline' https://togovar.github.io",
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://togostanza.github.io",
-  "font-src 'self' https://fonts.gstatic.com https://togostanza.github.io data:",
-  "img-src 'self' data: https:",
-  "connect-src 'self' https:",
-].join('; ');
+function buildCspDirectives(nonce) {
+  return [
+    "default-src 'self'",
+    "base-uri 'self'",
+    "object-src 'none'",
+    "frame-ancestors 'none'",
+    "form-action 'self'",
+    `script-src 'self' 'nonce-${nonce}' https://togovar.github.io`,
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://togostanza.github.io",
+    "font-src 'self' https://fonts.gstatic.com https://togostanza.github.io data:",
+    "img-src 'self' data: https:",
+    "connect-src 'self' https:",
+  ].join('; ');
+}
 
 function setSecurityHeaders(req, res, next) {
-  res.setHeader('Content-Security-Policy', CSP_DIRECTIVES);
+  const nonce = crypto.randomBytes(16).toString('base64');
+  res.locals.cspNonce = nonce;
+
+  res.setHeader('Content-Security-Policy', buildCspDirectives(nonce));
   res.setHeader(
     'Strict-Transport-Security',
     `max-age=${ONE_YEAR_SECONDS}; includeSubDomains`
