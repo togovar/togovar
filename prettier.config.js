24177ed02a92a083a4f2dff5ecf50a5695dad8bf
Yukiko Noda <yukiko8318@gmail.com>
2026-06-11 14:48:05 +0900

    prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
