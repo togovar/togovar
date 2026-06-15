d2aabfd614834b60017b3b720dbdc6509b179cf2
Yukiko Noda <yukiko8318@gmail.com>
2026-06-15 10:54:18 +0900

    fix webpack.config.dev.js

--- app/frontend/config/webpack.config.dev.js
+++ app/frontend/config/webpack.config.dev.js
@@ -1,10 +1,24 @@
 import { merge } from 'webpack-merge';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import commonConfig from './webpack.config.common.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: 'dist',
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/variant(?:\/.*)?$/, to: '/variant/index.html' },
+        { from: /^\/gene(?:\/.*)?$/, to: '/gene/index.html' },
+        { from: /^\/disease(?:\/.*)?$/, to: '/disease/index.html' },
+      ],
+    },
+    static: {
+      directory: path.resolve(__dirname, '../../../dist'),
+    },
   },
 });
