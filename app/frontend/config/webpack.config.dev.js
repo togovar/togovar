fde38d3a8202d5fad6efc9f8d9b1e55ec0cbedbf
Yukiko Noda <yukiko8318@gmail.com>
2026-06-15 12:03:45 +0900

    fix webpack.config.dev.js

--- app/frontend/config/webpack.config.dev.js
+++ app/frontend/config/webpack.config.dev.js
@@ -1,10 +1,5 @@
 import { merge } from 'webpack-merge';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import commonConfig from './webpack.config.common.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default merge(commonConfig, {
   mode: 'development',
@@ -17,8 +12,6 @@ export default merge(commonConfig, {
         { from: /^\/disease(?:\/.*)?$/, to: '/disease/index.html' },
       ],
     },
-    static: {
-      directory: path.resolve(__dirname, '../../../dist'),
-    },
+    static: false,
   },
 });
